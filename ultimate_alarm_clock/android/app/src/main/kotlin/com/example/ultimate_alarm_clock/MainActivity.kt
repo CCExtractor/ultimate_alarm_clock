c6a97e56650b27ef284ad005a07720786b67a8d0
Rijuth Menon <rijuthm@gmail.com>
2023-03-16 13:36:36 +0530

    Initial commit

---
+++ ultimate_alarm_clock/android/app/src/main/kotlin/com/example/ultimate_alarm_clock/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ultimate_alarm_clock
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ ultimate_alarm_clock/lib/app/modules/home/views/home_view.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+
+import '../controllers/home_controller.dart';
+
+class HomeView extends GetView<HomeController> {
+  const HomeView({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('HomeView'),
+        centerTitle: true,
+      ),
+      body: const Center(
+        child: Text(
+          'HomeView is working',
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
