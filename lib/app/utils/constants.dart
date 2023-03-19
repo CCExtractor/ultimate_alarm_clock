2138c569e3091111b264abda5da2de081731170a
Rijuth Menon <rijuthm@gmail.com>
2023-03-19 13:51:55 +0530

    added default font and scaffoldBackgroundColor

--- lib/app/utils/constants.dart
+++ lib/app/utils/constants.dart
@@ -7,30 +7,27 @@ const Color ksecondaryBackgroundColor = Color(0xff1c1f26);
 const Color kprimaryTextColor = Colors.white;
 const Color kprimaryDisabledTextColor = Color(0xff595f6b);
 ThemeData kThemeData = ThemeData(
+    fontFamily: 'poppins',
     textTheme: const TextTheme(
-        bodySmall: TextStyle(
-            color: kprimaryTextColor,
-            fontFamily: 'poppins',
-            letterSpacing: 0.15),
-        bodyMedium: TextStyle(
-            color: kprimaryTextColor,
-            fontFamily: 'poppins',
-            letterSpacing: 0.15),
+        titleSmall: TextStyle(color: kprimaryTextColor, letterSpacing: 0.15),
+        bodySmall: TextStyle(color: kprimaryTextColor, letterSpacing: 0.15),
+        bodyMedium: TextStyle(color: kprimaryTextColor, letterSpacing: 0.15),
+        bodyLarge: TextStyle(color: kprimaryTextColor, letterSpacing: 0.15),
         displayMedium: TextStyle(
             fontSize: 28,
             color: kprimaryTextColor,
-            fontFamily: 'poppins',
             fontWeight: FontWeight.w600,
             letterSpacing: 0.15),
         displaySmall: TextStyle(
             fontSize: 23,
             color: kprimaryTextColor,
-            fontFamily: 'poppins',
             fontWeight: FontWeight.w600,
             letterSpacing: 0.15)),
     floatingActionButtonTheme: FloatingActionButtonThemeData(
         foregroundColor: Colors.black, backgroundColor: kprimaryColor),
     primaryColor: kprimaryColor,
+    scaffoldBackgroundColor: kprimaryBackgroundColor,
+    appBarTheme: const AppBarTheme(backgroundColor: kprimaryBackgroundColor),
     colorScheme: ColorScheme.fromSwatch().copyWith(
         secondary: ksecondaryColor,
         background: kprimaryBackgroundColor,
