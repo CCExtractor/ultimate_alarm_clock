cf23b0ee87fd27e91ea55cecb6f88b9a4eade6b7
Rijuth Menon <rijuthm@gmail.com>
2023-03-19 07:19:43 +0530

    added contants

---
+++ lib/app/utils/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+Color kprimaryColor = Color(0xffAFFC41);
+Color ksecondaryColor = Color(0xffB8E9C4);
+Color kprimaryBackgroundColor = Color.fromARGB(255, 0, 20, 58);
+Color ksecondaryBackgroundColor = Color(0xff1d1d1d);
