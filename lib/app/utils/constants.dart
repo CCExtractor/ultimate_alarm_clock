746da37d254921ef40aac225982edbd48bc85684
Rijuth Menon <rijuthm@gmail.com>
2023-03-19 09:12:57 +0530

    added constants for easier theme switch

--- lib/app/utils/constants.dart
+++ lib/app/utils/constants.dart
@@ -1,27 +1,33 @@
 import 'package:flutter/material.dart';
 
-Color kprimaryColor = Color(0xffAFFC41);
-Color ksecondaryColor = Color(0xffB8E9C4);
-Color kprimaryBackgroundColor = Color(0xff16171c);
-Color ksecondaryBackgroundColor = Color(0xff1c1f26);
-
+const Color kprimaryColor = Color(0xffAFFC41);
+const Color ksecondaryColor = Color(0xffB8E9C4);
+const Color kprimaryBackgroundColor = Color(0xff16171c);
+const Color ksecondaryBackgroundColor = Color(0xff1c1f26);
+const Color kprimaryTextColor = Colors.white;
 ThemeData kThemeData = ThemeData(
     textTheme: const TextTheme(
+        bodySmall: TextStyle(
+            color: kprimaryTextColor,
+            fontFamily: 'poppins',
+            letterSpacing: 0.15),
         displayMedium: TextStyle(
-            fontSize: 21,
-            color: Colors.white,
+            fontSize: 25,
+            color: kprimaryTextColor,
             fontFamily: 'poppins',
             fontWeight: FontWeight.w600,
             letterSpacing: 0.15),
         displaySmall: TextStyle(
             fontSize: 21,
-            color: Colors.white,
+            color: kprimaryTextColor,
             fontFamily: 'poppins',
             fontWeight: FontWeight.w600,
             letterSpacing: 0.15)),
     floatingActionButtonTheme: FloatingActionButtonThemeData(
         foregroundColor: Colors.black, backgroundColor: kprimaryColor),
     primaryColor: kprimaryColor,
+    cardColor: ksecondaryBackgroundColor,
+    canvasColor: ksecondaryBackgroundColor,
     colorScheme: ColorScheme.fromSwatch().copyWith(
         secondary: ksecondaryColor,
         background: kprimaryBackgroundColor,
