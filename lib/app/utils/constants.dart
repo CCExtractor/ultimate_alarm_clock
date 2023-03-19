498947bee0d2cdf31f82c3fd26c9940002a13b60
Rijuth Menon <rijuthm@gmail.com>
2023-03-19 08:12:08 +0530

    added theme to maintain colors

--- lib/app/utils/constants.dart
+++ lib/app/utils/constants.dart
@@ -2,5 +2,14 @@ import 'package:flutter/material.dart';
 
 Color kprimaryColor = Color(0xffAFFC41);
 Color ksecondaryColor = Color(0xffB8E9C4);
-Color kprimaryBackgroundColor = Color.fromARGB(255, 0, 20, 58);
-Color ksecondaryBackgroundColor = Color(0xff1d1d1d);
+Color kprimaryBackgroundColor = Color(0xff16171c);
+Color ksecondaryBackgroundColor = Color(0xff1c1f26);
+
+ThemeData kThemeData = ThemeData(
+    floatingActionButtonTheme: FloatingActionButtonThemeData(
+        foregroundColor: Colors.black, backgroundColor: kprimaryColor),
+    primaryColor: kprimaryColor,
+    colorScheme: ColorScheme.fromSwatch().copyWith(
+        secondary: ksecondaryColor,
+        background: kprimaryBackgroundColor,
+        onPrimaryContainer: ksecondaryBackgroundColor));
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:ultimate_alarm_clock/app/utils/constants.dart';
 
 import 'app/routes/app_pages.dart';
 
 void main() {
   runApp(
     GetMaterialApp(
+      theme: kThemeData,
       title: "UltiClock",
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
