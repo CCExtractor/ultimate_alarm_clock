5b093948f6edd423cc20ed87d34ecd848c910100
Rijuth Menon <rijuthm@gmail.com>
2023-03-19 16:52:45 +0530

    object box init code

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:ultimate_alarm_clock/app/data/models/providers/objectbox.dart';
 import 'package:ultimate_alarm_clock/app/utils/constants.dart';
 
 import 'app/routes/app_pages.dart';
 
-void main() {
+late ObjectBox objectbox;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  objectbox = await ObjectBox.init();
   runApp(
     GetMaterialApp(
       theme: kThemeData,
