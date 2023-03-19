48fa73f34abc0ed9f582c96335c42d95f4ca8265
Rijuth Menon <rijuthm@gmail.com>
2023-03-19 16:51:44 +0530

    added object box

---
+++ lib/app/data/models/providers/objectbox.dart
@@ -0,0 +1,32 @@
+import 'package:path/path.dart' as p;
+import 'package:path_provider/path_provider.dart';
+import 'package:ultimate_alarm_clock/app/data/models/alarm_handler_model.dart';
+
+import '../../../../objectbox.g.dart';
+
+class ObjectBox {
+  /// The Store of this app.
+  late final Store _store;
+  late final Box<AlarmModel> _alarmBox;
+
+  ObjectBox._init(this._store) {
+    _alarmBox = Box<AlarmModel>(_store);
+  }
+
+  static Future<ObjectBox> init() async {
+    final store = await openStore();
+
+    return ObjectBox._init(store);
+  }
+
+  AlarmModel? getAlarm(int id) => _alarmBox.get(id);
+
+  Stream<List<AlarmModel>> getAlarms() => _alarmBox
+      .query()
+      .watch(triggerImmediately: true)
+      .map((query) => query.find());
+
+  int insertAlarm(AlarmModel alarmRecord) => _alarmBox.put(alarmRecord);
+
+  bool deleteUser(int id) => _alarmBox.remove(id);
+}
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,11 +5,13 @@
 import FlutterMacOS
 import Foundation
 
+import objectbox_flutter_libs
 import path_provider_foundation
 import shared_preferences_foundation
 import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ObjectboxFlutterLibsPlugin.register(with: registry.registrar(forPlugin: "ObjectboxFlutterLibsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
