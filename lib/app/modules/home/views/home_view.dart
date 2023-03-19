211a33944a25b33f9da7eb90d8f5807311fa8a37
Rijuth Menon <rijuthm@gmail.com>
2023-03-19 13:20:29 +0530

    increased container size

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -34,7 +34,7 @@ class HomeView extends GetView<HomeController> {
                       return Center(
                         child: Container(
                           width: width * 0.91,
-                          height: height * 0.10,
+                          height: height * 0.135,
                           decoration: BoxDecoration(
                               color: ksecondaryBackgroundColor,
                               borderRadius:
@@ -94,50 +94,55 @@ class HomeView extends GetView<HomeController> {
                                     padding: const EdgeInsets.symmetric(
                                         horizontal: 10.0),
                                     child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
                                       children: [
                                         Expanded(
-                                          flex: 2,
+                                          flex: 0,
                                           child: Switch(
                                               value: true,
                                               onChanged: (bool value) {}),
                                         ),
-                                        PopupMenuButton(
-                                            color: kprimaryBackgroundColor,
-                                            icon: const Icon(
-                                              Icons.more_vert,
-                                              color: kprimaryTextColor,
-                                            ),
-                                            itemBuilder: (context) {
-                                              return [
-                                                PopupMenuItem<int>(
-                                                  value: 0,
-                                                  child: Text(
-                                                    "Preview Alarm",
-                                                    style: Theme.of(context)
-                                                        .textTheme
-                                                        .bodyMedium,
+                                        Expanded(
+                                          flex: 0,
+                                          child: PopupMenuButton(
+                                              color: kprimaryBackgroundColor,
+                                              icon: const Icon(
+                                                Icons.more_vert,
+                                                color: kprimaryTextColor,
+                                              ),
+                                              itemBuilder: (context) {
+                                                return [
+                                                  PopupMenuItem<int>(
+                                                    value: 0,
+                                                    child: Text(
+                                                      "Preview Alarm",
+                                                      style: Theme.of(context)
+                                                          .textTheme
+                                                          .bodyMedium,
+                                                    ),
                                                   ),
-                                                ),
-                                                PopupMenuItem<int>(
-                                                  value: 1,
-                                                  child: Text(
-                                                    "Delete Alarm",
-                                                    style: Theme.of(context)
-                                                        .textTheme
-                                                        .bodyMedium,
+                                                  PopupMenuItem<int>(
+                                                    value: 1,
+                                                    child: Text(
+                                                      "Delete Alarm",
+                                                      style: Theme.of(context)
+                                                          .textTheme
+                                                          .bodyMedium,
+                                                    ),
                                                   ),
-                                                ),
-                                              ];
-                                            },
-                                            onSelected: (value) {
-                                              if (value == 0) {
-                                                print(
-                                                    "Preview menu is selected.");
-                                              } else if (value == 1) {
-                                                print(
-                                                    "Delete menu is selected.");
-                                              }
-                                            })
+                                                ];
+                                              },
+                                              onSelected: (value) {
+                                                if (value == 0) {
+                                                  print(
+                                                      "Preview menu is selected.");
+                                                } else if (value == 1) {
+                                                  print(
+                                                      "Delete menu is selected.");
+                                                }
+                                              }),
+                                        )
                                       ],
                                     ),
                                   ),
--- lib/app/utils/constants.dart
+++ lib/app/utils/constants.dart
@@ -5,6 +5,7 @@ const Color ksecondaryColor = Color(0xffB8E9C4);
 const Color kprimaryBackgroundColor = Color(0xff16171c);
 const Color ksecondaryBackgroundColor = Color(0xff1c1f26);
 const Color kprimaryTextColor = Colors.white;
+const Color kprimaryDisabledTextColor = Color(0xff595f6b);
 ThemeData kThemeData = ThemeData(
     textTheme: const TextTheme(
         bodySmall: TextStyle(
@@ -16,13 +17,13 @@ ThemeData kThemeData = ThemeData(
             fontFamily: 'poppins',
             letterSpacing: 0.15),
         displayMedium: TextStyle(
-            fontSize: 25,
+            fontSize: 28,
             color: kprimaryTextColor,
             fontFamily: 'poppins',
             fontWeight: FontWeight.w600,
             letterSpacing: 0.15),
         displaySmall: TextStyle(
-            fontSize: 21,
+            fontSize: 23,
             color: kprimaryTextColor,
             fontFamily: 'poppins',
             fontWeight: FontWeight.w600,
