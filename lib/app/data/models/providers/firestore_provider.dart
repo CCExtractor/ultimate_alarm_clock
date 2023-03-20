1d9126cabca94053fb170eaf0e3c9599f08f915a
Rijuth Menon <rijuthm@gmail.com>
2023-03-20 21:25:29 +0530

    created FirestoreDb class

--- lib/app/data/models/providers/firestore_provider.dart
+++ lib/app/data/models/providers/firestore_provider.dart
@@ -1,6 +1,27 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:ultimate_alarm_clock/app/data/models/alarm_model.dart';
 
 class FirestoreDb {
   static final FirebaseFirestore _firebaseFirestore =
       FirebaseFirestore.instance;
+  static final CollectionReference _alarmsCollection =
+      _firebaseFirestore.collection('alarms');
+
+  static addAlarm(AlarmModel alarmRecord) async {
+    late String id;
+    await _alarmsCollection
+        .add(AlarmModel.toMap(alarmRecord))
+        .then((value) => id = value.id);
+    return id;
+  }
+
+  static updateAlarm(String id, AlarmModel alarmRecord) async =>
+      await _alarmsCollection.doc(id).update(AlarmModel.toMap(alarmRecord));
+
+  static getAlarm(String id) => _alarmsCollection.doc(id).get();
+
+  static getAlarms() => _firebaseFirestore.collection('alarms').snapshots();
+
+  static deleteAlarm(String id) async =>
+      await _alarmsCollection.doc(id).delete();
 }
--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -6,9 +6,6 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <objectbox_flutter_libs/objectbox_flutter_libs_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
-  ObjectboxFlutterLibsPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("ObjectboxFlutterLibsPlugin"));
 }
