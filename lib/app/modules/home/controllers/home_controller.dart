b30c835ee8156819b2063768eea3a4d57f5e0c7a
Rijuth Menon <rijuthm@gmail.com>
2023-03-20 21:26:00 +0530

    migrated to firestore in home controller

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,15 +1,17 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
 import 'package:ultimate_alarm_clock/app/data/models/alarm_model.dart';
+import 'package:ultimate_alarm_clock/app/data/models/providers/firestore_provider.dart';
 import 'package:ultimate_alarm_clock/main.dart';
 
 class HomeController extends GetxController {
-  late Stream<List<AlarmModel>> streamAlarms;
+  late Stream<QuerySnapshot> streamAlarms;
 
   final count = 0.obs;
   @override
   void onInit() {
     super.onInit();
-    streamAlarms = objectbox.getAlarms();
+    streamAlarms = FirestoreDb.getAlarms();
   }
 
   @override
