eb7d489189834fcf715dc15e0e40c330126e8d8c
Rijuth Menon <rijuthm@gmail.com>
2023-03-20 21:26:19 +0530

    added new constructor and static method for firebase

--- lib/app/data/models/alarm_model.dart
+++ lib/app/data/models/alarm_model.dart
@@ -1,19 +1,38 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:objectbox/objectbox.dart';
 
 @Entity()
 class AlarmModel {
-  @Id()
-  int id;
-  String alarmTime;
-  bool isEnabled;
-  int intervalToAlarm;
-  bool isActivityEnabled;
-  int activityInterval;
+  String? id;
+  late String alarmTime;
+  late bool isEnabled;
+  late int intervalToAlarm;
+  late bool isActivityEnabled;
+  int? activityInterval;
   AlarmModel(
-      {this.id = 0,
-      required this.alarmTime,
+      {required this.alarmTime,
       this.isEnabled = true,
       required this.intervalToAlarm,
       required this.isActivityEnabled,
       this.activityInterval = 600000});
+
+  AlarmModel.fromDocumentSnapshot(
+      {required DocumentSnapshot documentSnapshot}) {
+    id = documentSnapshot.id;
+    alarmTime = documentSnapshot['alarmTime'];
+    isEnabled = documentSnapshot['isEnabled'];
+    intervalToAlarm = documentSnapshot['intervalToAlarm'];
+    isActivityEnabled = documentSnapshot['isActivityEnabled'];
+    activityInterval = documentSnapshot['activityInterval'];
+  }
+
+  static Map<String, dynamic> toMap(AlarmModel alarmRecord) {
+    return {
+      'alarmTime': alarmRecord.alarmTime,
+      'intervalToAlarm': alarmRecord.intervalToAlarm,
+      'isEnabled': alarmRecord.isEnabled,
+      'isActivityEnabled': alarmRecord.isActivityEnabled,
+      'activityInterval': alarmRecord.activityInterval
+    };
+  }
 }
