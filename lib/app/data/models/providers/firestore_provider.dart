44ffcaf0ad01d085bcd716e1077f6bb31b03df7c
Rijuth Menon <rijuthm@gmail.com>
2023-03-21 21:51:05 +0530

    returning alarmRecord from addAlarm function

--- lib/app/data/models/providers/firestore_provider.dart
+++ lib/app/data/models/providers/firestore_provider.dart
@@ -8,18 +8,17 @@ class FirestoreDb {
       _firebaseFirestore.collection('alarms');
 
   static addAlarm(AlarmModel alarmRecord) async {
-    late String id;
     await _alarmsCollection
         .add(AlarmModel.toMap(alarmRecord))
-        .then((value) => id = value.id);
-    return id;
+        .then((value) => alarmRecord.id = value.id);
+    return alarmRecord;
   }
 
   static updateAlarm(AlarmModel alarmRecord) async => await _alarmsCollection
       .doc(alarmRecord.id)
       .update(AlarmModel.toMap(alarmRecord));
 
-  static getAlarm(String id) => _alarmsCollection.doc(id).get();
+  static getAlarm(String id) async => await _alarmsCollection.doc(id).get();
 
   static getAlarms() => _firebaseFirestore.collection('alarms').snapshots();
 
