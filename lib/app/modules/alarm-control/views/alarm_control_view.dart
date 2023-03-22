ba95b2739fec53aa6bc7833d318b0f4832803257
Rijuth Menon <rijuthm@gmail.com>
2023-03-22 09:42:21 +0530

    added UI for control view

--- lib/app/modules/alarm-control/views/alarm_control_view.dart
+++ lib/app/modules/alarm-control/views/alarm_control_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:ultimate_alarm_clock/app/utils/constants.dart';
 
 import '../controllers/alarm_control_controller.dart';
 
@@ -8,17 +9,77 @@ class AlarmControlView extends GetView<AlarmControlController> {
   const AlarmControlView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('AlarmControlView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'AlarmControlView is working',
-          style: TextStyle(fontSize: 20),
-        ),
-      ),
+    var width = Get.width;
+    var height = Get.height;
+    return SafeArea(
+      child: Scaffold(
+          floatingActionButtonLocation:
+              FloatingActionButtonLocation.centerDocked,
+          floatingActionButton: Padding(
+            padding: EdgeInsets.all(18.0),
+            child: SizedBox(
+              height: height * 0.06,
+              width: width * 0.8,
+              child: TextButton(
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(kprimaryColor)),
+                child: Text(
+                  'Dismiss',
+                  style: Theme.of(context)
+                      .textTheme
+                      .displaySmall!
+                      .copyWith(color: ksecondaryTextColor),
+                ),
+                onPressed: () {
+                  Get.back();
+                },
+              ),
+            ),
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Obx(
+                  () => Column(
+                    children: [
+                      Text(
+                        controller.formattedDate.value,
+                        style: Theme.of(context).textTheme.bodyLarge,
+                      ),
+                      const SizedBox(
+                        height: 10,
+                        width: 0,
+                      ),
+                      Text(
+                        "${controller.timeNow[0]} ${controller.timeNow[1]}",
+                        style: Theme.of(context)
+                            .textTheme
+                            .displayLarge!
+                            .copyWith(fontSize: 50),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: height * 0.055,
+                  width: width * 0.25,
+                  child: TextButton(
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(
+                            ksecondaryBackgroundColor)),
+                    child: Text(
+                      'Snooze',
+                      style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                          color: kprimaryTextColor,
+                          fontWeight: FontWeight.w600),
+                    ),
+                    onPressed: () {},
+                  ),
+                )
+              ],
+            ),
+          )),
     );
   }
 }
