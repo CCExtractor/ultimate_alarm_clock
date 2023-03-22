0f9383b42fbe70aac91d1c4eea8963988fe6498b
Rijuth Menon <rijuthm@gmail.com>
2023-03-22 09:42:14 +0530

    added timer to update time on control view

--- lib/app/modules/alarm-control/controllers/alarm_control_controller.dart
+++ lib/app/modules/alarm-control/controllers/alarm_control_controller.dart
@@ -1,9 +1,25 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:ultimate_alarm_clock/app/utils/utils.dart';
 
 class AlarmControlController extends GetxController {
+  final formattedDate = Utils.getFormattedDate(DateTime.now()).obs;
+  final timeNow =
+      Utils.convertTo12HourFormat(Utils.timeOfDayToString(TimeOfDay.now())).obs;
   @override
   void onInit() {
     super.onInit();
+    Timer.periodic(
+        Duration(
+            milliseconds: Utils.getMillisecondsToAlarm(DateTime.now(),
+                DateTime.now().add(const Duration(minutes: 1)))), (timer) {
+      formattedDate.value = Utils.getFormattedDate(DateTime.now());
+      timeNow.value =
+          Utils.convertTo12HourFormat(Utils.timeOfDayToString(TimeOfDay.now()))
+              .obs;
+    });
   }
 
   @override
