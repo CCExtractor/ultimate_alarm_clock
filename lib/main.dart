d4f1b37268cd9bd0358ab6253d0e6df7206af1c3
Rijuth Menon <rijuthm@gmail.com>
2023-03-23 01:12:56 +0530

    removed objectbox comment code from main

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,12 @@ import 'package:firebase_core/firebase_core.dart';
 
 import 'package:get/get.dart';
 import 'package:ultimate_alarm_clock/app/utils/constants.dart';
-
 import 'app/routes/app_pages.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(); // objectbox = await ObjectBox.init();
-  // objectbox.deleteAllAlarms();
+
+  await Firebase.initializeApp();
   runApp(
     GetMaterialApp(
       theme: kThemeData,
