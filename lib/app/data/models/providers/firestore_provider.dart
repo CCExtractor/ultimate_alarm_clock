6bde397246f2e0ec7e5ef2b744b78488c569063d
Rijuth Menon <rijuthm@gmail.com>
2023-03-22 16:26:47 +0530

    added function to get latest alarmModel to schedule and sorted the entries

--- lib/app/data/models/providers/firestore_provider.dart
+++ lib/app/data/models/providers/firestore_provider.dart
@@ -14,13 +14,29 @@ class FirestoreDb {
     return alarmRecord;
   }
 
+  static getLatestAlarm(AlarmModel alarmRecord) async {
+    final QuerySnapshot snapshot = await _alarmsCollection
+        .where('isEnabled', isEqualTo: true)
+        .where('minutesSinceMidnight',
+            isLessThan: alarmRecord.minutesSinceMidnight)
+        .orderBy('minutesSinceMidnight', descending: false)
+        .get();
+    final list = snapshot.docs.map((DocumentSnapshot document) {
+      return AlarmModel.fromDocumentSnapshot(documentSnapshot: document);
+    }).toList();
+    if (list.isEmpty) return alarmRecord;
+    return list[0];
+  }
+
   static updateAlarm(AlarmModel alarmRecord) async => await _alarmsCollection
       .doc(alarmRecord.id)
       .update(AlarmModel.toMap(alarmRecord));
 
   static getAlarm(String id) async => await _alarmsCollection.doc(id).get();
 
-  static getAlarms() => _firebaseFirestore.collection('alarms').snapshots();
+  static getAlarms() => _alarmsCollection
+      .orderBy('minutesSinceMidnight', descending: false)
+      .snapshots();
 
   static deleteAlarm(String id) async =>
       await _alarmsCollection.doc(id).delete();
