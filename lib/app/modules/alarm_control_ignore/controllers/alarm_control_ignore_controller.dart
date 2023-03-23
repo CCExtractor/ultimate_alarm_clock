ca028e3d554c92f974cd427011d725ef0ed67fa4
Rijuth Menon <rijuthm@gmail.com>
2023-03-23 22:51:13 +0530

    redirecting to home to skip ignore view

--- lib/app/modules/alarm_control_ignore/controllers/alarm_control_ignore_controller.dart
+++ lib/app/modules/alarm_control_ignore/controllers/alarm_control_ignore_controller.dart
@@ -4,13 +4,14 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:ultimate_alarm_clock/app/utils/utils.dart';
 import 'package:app_minimizer/app_minimizer.dart';
+import 'package:flutter_fgbg/flutter_fgbg.dart';
 
 class AlarmControlIgnoreController extends GetxController {
   final formattedDate = Utils.getFormattedDate(DateTime.now()).obs;
   final timeNow =
       Utils.convertTo12HourFormat(Utils.timeOfDayToString(TimeOfDay.now())).obs;
   @override
-  void onInit() {
+  void onInit() async {
     super.onInit();
     Timer.periodic(
         Duration(
@@ -21,6 +22,12 @@ class AlarmControlIgnoreController extends GetxController {
           Utils.convertTo12HourFormat(Utils.timeOfDayToString(TimeOfDay.now()));
     });
     FlutterAppMinimizer.minimize();
+
+    FGBGType status = await FGBGEvents.stream.first;
+
+    if (status == FGBGType.foreground) {
+      Get.offNamed('/home');
+    }
   }
 
   @override
