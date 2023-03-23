4bc77ff683d4200628940d40881ffecb26feb958
Rijuth Menon <rijuthm@gmail.com>
2023-03-23 21:29:48 +0530

    added functions to get permissions for location

--- lib/app/modules/addAlarm/controllers/add_alarm_controller.dart
+++ lib/app/modules/addAlarm/controllers/add_alarm_controller.dart
@@ -3,13 +3,11 @@ import 'dart:isolate';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:get/get.dart';
-import 'package:flutter_ringtone_player/flutter_ringtone_player.dart';
-
+import 'package:fl_location/fl_location.dart';
 import 'package:flutter_foreground_task/flutter_foreground_task.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:ultimate_alarm_clock/app/data/models/alarm_handler_model.dart';
 
-import 'package:path/path.dart';
 import 'package:ultimate_alarm_clock/app/data/models/alarm_model.dart';
 import 'package:ultimate_alarm_clock/app/data/models/providers/firestore_provider.dart';
 import 'package:ultimate_alarm_clock/app/utils/utils.dart';
@@ -29,6 +27,43 @@ class AddAlarmController extends GetxController {
   final MapController mapController = MapController();
   final selectedPoint = LatLng(0, 0).obs;
   final List<Marker> markersList = [];
+
+  Future<void> getLocation() async {
+    if (await _checkAndRequestPermission()) {
+      final timeLimit = const Duration(seconds: 10);
+      await FlLocation.getLocation(timeLimit: timeLimit).then((location) {
+        print('location: ${location.toJson().toString()}');
+      }).onError((error, stackTrace) {
+        print('error: ${error.toString()}');
+      });
+    }
+  }
+
+  Future<bool> _checkAndRequestPermission({bool? background}) async {
+    if (!await FlLocation.isLocationServicesEnabled) {
+      // Location services are disabled.
+      return false;
+    }
+
+    var locationPermission = await FlLocation.checkLocationPermission();
+    if (locationPermission == LocationPermission.deniedForever) {
+      // Cannot request runtime permission because location permission is denied forever.
+      return false;
+    } else if (locationPermission == LocationPermission.denied) {
+      // Ask the user for location permission.
+      locationPermission = await FlLocation.requestLocationPermission();
+      if (locationPermission == LocationPermission.denied ||
+          locationPermission == LocationPermission.deniedForever) return false;
+    }
+
+    // Location permission must always be allowed (LocationPermission.always)
+    // to collect location data in the background.
+    if (background == true &&
+        locationPermission == LocationPermission.whileInUse) return false;
+
+    // Location services has been enabled and permission have been granted.
+    return true;
+  }
 
   createAlarm(AlarmModel alarmData) async {
     _alarmRecord = await FirestoreDb.addAlarm(alarmData);
