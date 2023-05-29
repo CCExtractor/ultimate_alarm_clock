63d39b1111ed9d6f0185b1f4fa799b90429c1d44
Rijuth Menon <rijuthm@gmail.com>
2023-05-29 07:39:26 +0530

    added code to schedule next latest alarm in ring screen

--- lib/app/modules/alarm-control/controllers/alarm_control_controller.dart
+++ lib/app/modules/alarm-control/controllers/alarm_control_controller.dart
@@ -1,16 +1,22 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_foreground_task/flutter_foreground_task.dart';
 import 'package:flutter_ringtone_player/flutter_ringtone_player.dart';
 import 'package:get/get.dart';
+import 'package:ultimate_alarm_clock/app/data/models/alarm_model.dart';
+import 'package:ultimate_alarm_clock/app/data/models/providers/alarm_handler_setup_model.dart';
+import 'package:ultimate_alarm_clock/app/data/models/providers/firestore_provider.dart';
+import 'package:ultimate_alarm_clock/app/data/models/providers/isar_provider.dart';
 import 'package:ultimate_alarm_clock/app/utils/utils.dart';
 
-class AlarmControlController extends GetxController {
+class AlarmControlController extends GetxController
+    with AlarmHandlerSetupModel {
   final formattedDate = Utils.getFormattedDate(DateTime.now()).obs;
   final timeNow =
       Utils.convertTo12HourFormat(Utils.timeOfDayToString(TimeOfDay.now())).obs;
   @override
-  void onInit() {
+  void onInit() async {
     super.onInit();
     FlutterRingtonePlayer.playAlarm();
     Timer.periodic(
@@ -21,6 +27,30 @@ class AlarmControlController extends GetxController {
       timeNow.value =
           Utils.convertTo12HourFormat(Utils.timeOfDayToString(TimeOfDay.now()));
     });
+
+    AlarmModel _alarmRecord = Utils.genFakeAlarmModel();
+
+    AlarmModel isarLatestAlarm = await IsarDb.getLatestAlarm(_alarmRecord);
+    AlarmModel firestoreLatestAlarm =
+        await FirestoreDb.getLatestAlarm(_alarmRecord);
+    AlarmModel latestAlarm =
+        Utils.getFirstScheduledAlarm(isarLatestAlarm, firestoreLatestAlarm);
+
+    TimeOfDay latestAlarmTimeOfDay =
+        Utils.stringToTimeOfDay(latestAlarm.alarmTime);
+
+    int intervaltoAlarm = Utils.getMillisecondsToAlarm(
+        DateTime.now(), Utils.timeOfDayToDateTime(latestAlarmTimeOfDay));
+    print("INTERVAL TO ALARM: ${intervaltoAlarm}");
+
+    if (await FlutterForegroundTask.isRunningService == false) {
+      // Starting service mandatorily!
+
+      createForegroundTask(intervaltoAlarm);
+      await startForegroundTask(latestAlarm);
+    } else {
+      await restartForegroundTask(latestAlarm, intervaltoAlarm);
+    }
   }
 
   @override
