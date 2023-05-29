18f8aca48ccaaa6418fe2b8abfa1a81fef68e2be
Rijuth Menon <rijuthm@gmail.com>
2023-05-29 07:39:46 +0530

    added code to choose next latest alarm in ignore ring screen

--- lib/app/modules/alarm_control_ignore/controllers/alarm_control_ignore_controller.dart
+++ lib/app/modules/alarm_control_ignore/controllers/alarm_control_ignore_controller.dart
@@ -1,12 +1,18 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_foreground_task/flutter_foreground_task.dart';
 import 'package:get/get.dart';
+import 'package:ultimate_alarm_clock/app/data/models/alarm_model.dart';
+import 'package:ultimate_alarm_clock/app/data/models/providers/alarm_handler_setup_model.dart';
+import 'package:ultimate_alarm_clock/app/data/models/providers/firestore_provider.dart';
+import 'package:ultimate_alarm_clock/app/data/models/providers/isar_provider.dart';
 import 'package:ultimate_alarm_clock/app/utils/utils.dart';
 import 'package:app_minimizer/app_minimizer.dart';
 import 'package:flutter_fgbg/flutter_fgbg.dart';
 
-class AlarmControlIgnoreController extends GetxController {
+class AlarmControlIgnoreController extends GetxController
+    with AlarmHandlerSetupModel {
   final formattedDate = Utils.getFormattedDate(DateTime.now()).obs;
   final timeNow =
       Utils.convertTo12HourFormat(Utils.timeOfDayToString(TimeOfDay.now())).obs;
@@ -21,6 +27,28 @@ class AlarmControlIgnoreController extends GetxController {
       timeNow.value =
           Utils.convertTo12HourFormat(Utils.timeOfDayToString(TimeOfDay.now()));
     });
+
+    AlarmModel _alarmRecord = Utils.genFakeAlarmModel();
+
+    AlarmModel isarLatestAlarm = await IsarDb.getLatestAlarm(_alarmRecord);
+    AlarmModel firestoreLatestAlarm =
+        await FirestoreDb.getLatestAlarm(_alarmRecord);
+    AlarmModel latestAlarm =
+        Utils.getFirstScheduledAlarm(isarLatestAlarm, firestoreLatestAlarm);
+
+    TimeOfDay latestAlarmTimeOfDay =
+        Utils.stringToTimeOfDay(latestAlarm.alarmTime);
+    int intervaltoAlarm = Utils.getMillisecondsToAlarm(
+        DateTime.now(), Utils.timeOfDayToDateTime(latestAlarmTimeOfDay));
+
+    if (await FlutterForegroundTask.isRunningService == false) {
+      // Starting service mandatorily!
+
+      createForegroundTask(intervaltoAlarm);
+      await startForegroundTask(latestAlarm);
+    } else {
+      await restartForegroundTask(latestAlarm, intervaltoAlarm);
+    }
     FlutterAppMinimizer.minimize();
 
     FGBGType status = await FGBGEvents.stream.first;
