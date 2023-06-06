919f59ac19b6ed14ae374164214da43573d88456
Rijuth Menon <rijuthm@gmail.com>
2023-06-06 06:09:15 +0530

    added theme for text input

--- lib/app/utils/constants.dart
+++ lib/app/utils/constants.dart
@@ -13,39 +13,58 @@ const Color ksecondaryTextColor = Colors.black;
 
 const Color kprimaryDisabledTextColor = Color(0xff595f6b);
 ThemeData kThemeData = ThemeData(
-    textButtonTheme: TextButtonThemeData(
-        style: TextButton.styleFrom(foregroundColor: ksecondaryColor)),
-    fontFamily: 'poppins',
-    checkboxTheme: CheckboxThemeData(
-      checkColor: MaterialStateProperty.all(kprimaryTextColor),
-      fillColor: MaterialStateProperty.all(kprimaryColor.withOpacity(0.8)),
-    ),
-    textTheme: const TextTheme(
-        titleSmall: TextStyle(color: kprimaryTextColor, letterSpacing: 0.15),
-        bodySmall: TextStyle(color: kprimaryTextColor, letterSpacing: 0.15),
-        bodyMedium: TextStyle(color: kprimaryTextColor, letterSpacing: 0.15),
-        bodyLarge: TextStyle(color: kprimaryTextColor, letterSpacing: 0.15),
-        displaySmall: TextStyle(
-            fontSize: 16,
-            color: kprimaryTextColor,
-            fontWeight: FontWeight.w600,
-            letterSpacing: 0.15),
-        displayLarge: TextStyle(
-            fontSize: 28,
-            color: kprimaryTextColor,
-            fontWeight: FontWeight.w600,
-            letterSpacing: 0.15),
-        displayMedium: TextStyle(
-            fontSize: 23,
-            color: kprimaryTextColor,
-            fontWeight: FontWeight.w600,
-            letterSpacing: 0.15)),
-    floatingActionButtonTheme: FloatingActionButtonThemeData(
-        foregroundColor: Colors.black, backgroundColor: kprimaryColor),
-    primaryColor: kprimaryColor,
-    scaffoldBackgroundColor: kprimaryBackgroundColor,
-    appBarTheme: const AppBarTheme(backgroundColor: kprimaryBackgroundColor),
-    colorScheme: ColorScheme.fromSwatch().copyWith(
-        secondary: ksecondaryColor,
-        background: kprimaryBackgroundColor,
-        onPrimaryContainer: ksecondaryBackgroundColor));
+  textButtonTheme: TextButtonThemeData(
+      style: TextButton.styleFrom(foregroundColor: ksecondaryColor)),
+  fontFamily: 'poppins',
+  checkboxTheme: CheckboxThemeData(
+    checkColor: MaterialStateProperty.all(kprimaryTextColor),
+    fillColor: MaterialStateProperty.all(kprimaryColor.withOpacity(0.8)),
+  ),
+  textTheme: const TextTheme(
+      titleSmall: TextStyle(color: kprimaryTextColor, letterSpacing: 0.15),
+      titleMedium: TextStyle(color: kprimaryTextColor, letterSpacing: 0.15),
+      bodySmall: TextStyle(color: kprimaryTextColor, letterSpacing: 0.15),
+      bodyMedium: TextStyle(color: kprimaryTextColor, letterSpacing: 0.15),
+      bodyLarge: TextStyle(color: kprimaryTextColor, letterSpacing: 0.15),
+      displaySmall: TextStyle(
+          fontSize: 16,
+          color: kprimaryTextColor,
+          fontWeight: FontWeight.w600,
+          letterSpacing: 0.15),
+      displayLarge: TextStyle(
+          fontSize: 28,
+          color: kprimaryTextColor,
+          fontWeight: FontWeight.w600,
+          letterSpacing: 0.15),
+      displayMedium: TextStyle(
+          fontSize: 23,
+          color: kprimaryTextColor,
+          fontWeight: FontWeight.w600,
+          letterSpacing: 0.15)),
+  floatingActionButtonTheme: FloatingActionButtonThemeData(
+      foregroundColor: Colors.black, backgroundColor: kprimaryColor),
+  primaryColor: kprimaryColor,
+  scaffoldBackgroundColor: kprimaryBackgroundColor,
+  appBarTheme: const AppBarTheme(backgroundColor: kprimaryBackgroundColor),
+  colorScheme: ColorScheme.fromSwatch().copyWith(
+      secondary: ksecondaryColor,
+      background: kprimaryBackgroundColor,
+      onPrimaryContainer: ksecondaryBackgroundColor),
+  inputDecorationTheme: InputDecorationTheme(
+      hintStyle: TextStyle(color: kprimaryTextColor),
+      labelStyle: TextStyle(color: kprimaryTextColor),
+      focusColor: kprimaryTextColor,
+      enabledBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: kprimaryColor),
+        borderRadius: BorderRadius.circular(12),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: kprimaryColor),
+        borderRadius: BorderRadius.circular(12),
+      )),
+  textSelectionTheme: TextSelectionThemeData(
+    cursorColor: kprimaryColor,
+    selectionColor: kprimaryColor,
+    selectionHandleColor: ksecondaryColor,
+  ),
+);
