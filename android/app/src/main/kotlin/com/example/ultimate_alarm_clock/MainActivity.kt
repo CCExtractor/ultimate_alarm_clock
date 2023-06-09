cf213cac0919078bccc50c8ee830abce53b6490f
Rijuth Menon <rijuthm@gmail.com>
2023-06-09 19:11:09 +0530

    added kotlin code to ensure app shows over lockscreen

--- android/app/src/main/kotlin/com/example/ultimate_alarm_clock/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/ultimate_alarm_clock/MainActivity.kt
@@ -1,6 +1,39 @@
 package com.example.ultimate_alarm_clock
 
+import android.content.Context
+import android.os.Bundle
+import android.os.PowerManager
+import android.view.WindowManager
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+    private val CHANNEL = "ultimate_alarm_clock_channel"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON or WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED or WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON)
+    }
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+            if (call.method == "acquireWakeLock") {
+                acquireWakeLock()
+                result.success(null)
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+
+    private fun acquireWakeLock() {
+        val powerManager = getSystemService(Context.POWER_SERVICE) as PowerManager
+        val wakeLock = powerManager.newWakeLock(
+            PowerManager.FULL_WAKE_LOCK or PowerManager.ACQUIRE_CAUSES_WAKEUP,
+            "UltimateAlarmClock:WakeLockTag"
+        )
+        wakeLock.acquire()
+    }
 }
