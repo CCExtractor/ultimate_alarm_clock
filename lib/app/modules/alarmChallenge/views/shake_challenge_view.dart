261b5f306a68a8200688ce8c816096dc5387b235
Rijuth Menon <rijuthm@gmail.com>
2023-06-12 07:32:30 +0530

    added shake to dismiss screen

---
+++ lib/app/modules/alarmChallenge/views/shake_challenge_view.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:ultimate_alarm_clock/app/utils/constants.dart';
+import 'dart:math' as math;
+
+import '../controllers/alarm_challenge_controller.dart';
+
+class ShakeChallengeView extends GetView<AlarmChallengeController> {
+  const ShakeChallengeView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var width = Get.width;
+    var height = Get.height;
+
+    return Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+        ),
+        body: GestureDetector(
+          onTap: () {
+            controller.restartTimer();
+          },
+          child: Column(
+            children: [
+              Obx(
+                () => LinearProgressIndicator(
+                  minHeight: 2,
+                  value: controller.progress.value,
+                  backgroundColor: Colors.grey,
+                  valueColor: AlwaysStoppedAnimation<Color>(kprimaryColor),
+                ),
+              ),
+              Expanded(
+                child: Center(
+                  child: SingleChildScrollView(
+                    child: Center(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 25.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              'Shake your phone!',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .displayMedium!
+                                  .copyWith(
+                                      fontWeight: FontWeight.w500,
+                                      color:
+                                          kprimaryTextColor.withOpacity(0.7)),
+                            ),
+                            SizedBox(
+                              height: height * 0.08,
+                            ),
+                            Transform.rotate(
+                              angle: -10 * math.pi / 180,
+                              child: Icon(
+                                Icons.vibration,
+                                size: height * 0.2,
+                                color: kprimaryTextColor.withOpacity(0.7),
+                              ),
+                            ),
+                            SizedBox(
+                              height: height * 0.08,
+                            ),
+                            Obx(
+                              () => Text(
+                                controller.shakedCount.value.toString(),
+                                style: TextStyle(fontSize: 35),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ));
+  }
+}
