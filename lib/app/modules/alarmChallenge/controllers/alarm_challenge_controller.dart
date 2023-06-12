355d3de3999bf7f868fed938a70978719001e52c
Rijuth Menon <rijuthm@gmail.com>
2023-06-12 07:31:47 +0530

    added shakeToDismiss and timeout progress bar

--- lib/app/modules/alarmChallenge/controllers/alarm_challenge_controller.dart
+++ lib/app/modules/alarmChallenge/controllers/alarm_challenge_controller.dart
@@ -1,28 +1,56 @@
 import 'dart:async';
 
 import 'package:get/get.dart';
+import 'package:shake/shake.dart';
+import 'package:ultimate_alarm_clock/app/data/models/alarm_model.dart';
+import 'package:ultimate_alarm_clock/app/utils/constants.dart';
 
 class AlarmChallengeController extends GetxController {
-  RxDouble progress = RxDouble(1.0);
+  AlarmModel alarmRecord = Get.arguments;
+  final RxDouble progress = 1.0.obs;
+  final RxInt shakedCount = 0.obs;
+  final isShakeOngoing = Status.initialized.obs;
+  ShakeDetector? _shakeDetector;
   @override
   void onInit() {
     super.onInit();
     _startTimer();
+
+    isShakeOngoing.listen((value) {
+      if (value == Status.ongoing) {
+        _shakeDetector = ShakeDetector.autoStart(onPhoneShake: () {
+          shakedCount.value -= 1;
+          restartTimer();
+        });
+      }
+    });
+
+    shakedCount.listen((value) {
+      if (value == 0) {
+        isShakeOngoing.value = Status.completed;
+        Get.back();
+        _shakeDetector!.stopListening();
+      }
+    });
   }
 
   void _startTimer() async {
     final duration = Duration(seconds: 15);
-    final totalIterations = 150000;
-    final decrement = 0.0001;
-    final interval = duration ~/ totalIterations;
+    final totalIterations = 1500000;
+    final decrement = 0.000001;
 
-    for (var i = 0; i < totalIterations; i++) {
+    for (var i = totalIterations; i > 0; i--) {
       if (progress.value <= 0.0) {
-        Get.back();
+        // Get.offAllNamed('/alarm-ring');
         break;
       }
-      await Future.delayed(interval);
+      await Future.delayed(duration ~/ i);
       progress.value -= decrement;
     }
+  }
+
+  restartTimer() {
+    progress.value = 1.0; // Reset the progress to its initial value
+    _startTimer(); // Start the timer again
   }
 }
