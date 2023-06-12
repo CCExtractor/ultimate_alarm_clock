8da9519823b06d61b67f43909b72277037553229
Rijuth Menon <rijuthm@gmail.com>
2023-06-12 08:57:56 +0530

    added qr code challenge logic

--- lib/app/modules/alarmChallenge/controllers/alarm_challenge_controller.dart
+++ lib/app/modules/alarmChallenge/controllers/alarm_challenge_controller.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:get/get.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
 import 'package:shake/shake.dart';
 import 'package:ultimate_alarm_clock/app/data/models/alarm_model.dart';
 import 'package:ultimate_alarm_clock/app/utils/constants.dart';
@@ -8,9 +9,24 @@ import 'package:ultimate_alarm_clock/app/utils/constants.dart';
 class AlarmChallengeController extends GetxController {
   AlarmModel alarmRecord = Get.arguments;
   final RxDouble progress = 1.0.obs;
+
   final RxInt shakedCount = 0.obs;
   final isShakeOngoing = Status.initialized.obs;
   ShakeDetector? _shakeDetector;
+  MobileScannerController? qrController;
+
+  final qrValue = "".obs;
+  final isQrOngoing = Status.initialized.obs;
+
+  restartQRCodeController() {
+    qrController = MobileScannerController(
+      autoStart: true,
+      detectionSpeed: DetectionSpeed.noDuplicates,
+      facing: CameraFacing.back,
+      torchEnabled: false,
+    );
+  }
+
   @override
   void onInit() {
     super.onInit();
@@ -30,6 +46,24 @@ class AlarmChallengeController extends GetxController {
         isShakeOngoing.value = Status.completed;
         Get.back();
         _shakeDetector!.stopListening();
+      }
+    });
+
+    if (alarmRecord.isQrEnabled) {
+      qrController = MobileScannerController(
+        autoStart: true,
+        detectionSpeed: DetectionSpeed.noDuplicates,
+        facing: CameraFacing.back,
+        torchEnabled: false,
+      );
+    }
+
+    qrValue.listen((value) {
+      restartTimer();
+      if (value == alarmRecord.qrValue) {
+        isQrOngoing.value = Status.completed;
+        qrController!.dispose();
+        Get.back();
       }
     });
   }
