5798c8ddd8cd9fa9237d0c80cefe50495bde3e29
Rijuth Menon <rijuthm@gmail.com>
2023-06-13 07:54:39 +0530

    fixed bug with infinite loading indicator

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -5,16 +5,19 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_foreground_task/flutter_foreground_task.dart';
 import 'package:get/get.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:ultimate_alarm_clock/app/data/models/alarm_handler_setup_model.dart';
 import 'package:ultimate_alarm_clock/app/data/models/alarm_model.dart';
+import 'package:ultimate_alarm_clock/app/data/models/user_model.dart';
 import 'package:ultimate_alarm_clock/app/data/providers/firestore_provider.dart';
 import 'package:ultimate_alarm_clock/app/data/providers/isar_provider.dart';
+import 'package:ultimate_alarm_clock/app/data/providers/secure_storage_provider.dart';
 import 'package:ultimate_alarm_clock/app/utils/utils.dart';
 
 class HomeController extends GetxController with AlarmHandlerSetupModel {
-  late Stream<QuerySnapshot> firestoreStreamAlarms;
-  late Stream isarStreamAlarms;
-  late Stream streamAlarms;
+  Stream<QuerySnapshot>? firestoreStreamAlarms;
+  Stream? isarStreamAlarms;
+  Stream? streamAlarms;
   List<AlarmModel> latestFirestoreAlarms = [];
   List<AlarmModel> latestIsarAlarms = [];
   final alarmTime = 'No upcoming alarms!'.obs;
@@ -24,15 +27,56 @@ class HomeController extends GetxController with AlarmHandlerSetupModel {
   List alarms = [].obs;
   int lastRefreshTime = DateTime.now().millisecondsSinceEpoch;
   Timer? delayToSchedule;
-  @override
-  void onInit() {
-    super.onInit();
-    firestoreStreamAlarms = FirestoreDb.getAlarms();
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+  UserModel? userModel;
+
+  loginWithGoogle() async {
+    // Logging in again to ensure right details if User has linked account
+    if (await SecureStorageProvider().retrieveUserModel() != null) {
+      if (await _googleSignIn.isSignedIn()) {
+        GoogleSignInAccount? googleSignInAccount =
+            await _googleSignIn.signInSilently();
+        String fullName = googleSignInAccount!.displayName.toString();
+        List<String> parts = fullName.split(" ");
+        String lastName = " ";
+        if (parts.length == 3) {
+          if (parts[parts.length - 1].length == 1) {
+            lastName = parts[1].toLowerCase().capitalizeFirst.toString();
+          } else {
+            lastName = parts[parts.length - 1]
+                .toLowerCase()
+                .capitalizeFirst
+                .toString();
+          }
+        } else {
+          lastName =
+              parts[parts.length - 1].toLowerCase().capitalizeFirst.toString();
+        }
+        String firstName = parts[0].toLowerCase().capitalizeFirst.toString();
+
+        userModel = UserModel(
+          id: googleSignInAccount.id,
+          fullName: fullName,
+          firstName: firstName,
+          lastName: lastName,
+          email: googleSignInAccount.email,
+        );
+        await SecureStorageProvider().storeUserModel(userModel!);
+      }
+    }
+  }
+
+  initStream() async {
+    await loginWithGoogle();
+    firestoreStreamAlarms = FirestoreDb.getAlarms(userModel);
     isarStreamAlarms = IsarDb.getAlarms();
 
-    streamAlarms = StreamGroup.merge([firestoreStreamAlarms, isarStreamAlarms])
-        .map((data) {
+    streamAlarms =
+        StreamGroup.merge([firestoreStreamAlarms!, isarStreamAlarms!])
+            .map((data) {
+      print(data.runtimeType);
       if (data is QuerySnapshot) {
+        print('firestore gave');
         List<DocumentSnapshot> firestoreDocuments = data.docs;
         latestFirestoreAlarms = firestoreDocuments
             .map((DocumentSnapshot doc) =>
@@ -101,6 +145,11 @@ class HomeController extends GetxController with AlarmHandlerSetupModel {
     refreshUpcomingAlarms();
   }
 
+  @override
+  void onInit() {
+    super.onInit();
+  }
+
   refreshUpcomingAlarms() async {
     // Check if 2 seconds have passed since the last call
     final currentTime = DateTime.now().millisecondsSinceEpoch;
@@ -127,7 +176,7 @@ class HomeController extends GetxController with AlarmHandlerSetupModel {
           await IsarDb.getLatestAlarm(alarmRecord, true);
 
       AlarmModel firestoreLatestAlarm =
-          await FirestoreDb.getLatestAlarm(alarmRecord, true);
+          await FirestoreDb.getLatestAlarm(userModel, alarmRecord, true);
       AlarmModel latestAlarm =
           Utils.getFirstScheduledAlarm(isarLatestAlarm, firestoreLatestAlarm);
 
