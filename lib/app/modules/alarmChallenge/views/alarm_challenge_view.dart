ec01a680a5f7d768d7e568addf705654762f67c3
Rijuth Menon <rijuthm@gmail.com>
2023-06-13 21:54:09 +0530

    displaying challenges only if enabled

--- lib/app/modules/alarmChallenge/views/alarm_challenge_view.dart
+++ lib/app/modules/alarmChallenge/views/alarm_challenge_view.dart
@@ -42,176 +42,167 @@ class AlarmChallengeView extends GetView<AlarmChallengeController> {
                       child: Column(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          InkWell(
-                            onTap: () {
-                              if (controller.isShakeOngoing.value !=
-                                      Status.completed &&
-                                  controller.alarmRecord.isShakeEnabled) {
-                                controller.shakedCount.value =
-                                    controller.alarmRecord.shakeTimes;
-                                controller.isShakeOngoing.value =
-                                    Status.ongoing;
-                                Get.to(() => ShakeChallengeView());
-                              }
-                            },
-                            child: Container(
-                              width: width * 0.91,
-                              height: height * 0.1,
-                              decoration: const BoxDecoration(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(18)),
-                                color: ksecondaryBackgroundColor,
-                              ),
-                              child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceAround,
-                                children: [
-                                  Icon(
-                                    Icons.vibration,
-                                    color: kprimaryTextColor.withOpacity(0.8),
-                                    size: 28,
-                                  ),
-                                  Text(
-                                    'Shake Challenge',
-                                    style: Theme.of(context)
-                                        .textTheme
-                                        .bodyLarge!
-                                        .copyWith(
-                                          color: kprimaryTextColor,
-                                        ),
-                                  ),
-                                  Obx(
-                                    () => Icon(
-                                      (controller.alarmRecord.isShakeEnabled)
-                                          ? controller.isShakeOngoing.value ==
-                                                  Status.completed
-                                              ? Icons.done
-                                              : Icons.arrow_forward_ios_sharp
-                                          : controller.isShakeOngoing.value ==
-                                                  Status.completed
-                                              ? Icons.done
-                                              : Icons.close,
-                                      color: kprimaryTextColor.withOpacity(0.2),
+                          if (controller.alarmRecord.isShakeEnabled)
+                            InkWell(
+                              onTap: () {
+                                if (controller.isShakeOngoing.value !=
+                                        Status.completed &&
+                                    controller.alarmRecord.isShakeEnabled) {
+                                  controller.shakedCount.value =
+                                      controller.alarmRecord.shakeTimes;
+                                  controller.isShakeOngoing.value =
+                                      Status.ongoing;
+                                  Get.to(() => ShakeChallengeView());
+                                }
+                              },
+                              child: Container(
+                                width: width * 0.91,
+                                height: height * 0.1,
+                                decoration: const BoxDecoration(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(18)),
+                                  color: ksecondaryBackgroundColor,
+                                ),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceAround,
+                                  children: [
+                                    Icon(
+                                      Icons.vibration,
+                                      color: kprimaryTextColor.withOpacity(0.8),
+                                      size: 28,
                                     ),
-                                  )
-                                ],
+                                    Text(
+                                      'Shake Challenge',
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .bodyLarge!
+                                          .copyWith(
+                                            color: kprimaryTextColor,
+                                          ),
+                                    ),
+                                    Obx(
+                                      () => Icon(
+                                        controller.isShakeOngoing.value ==
+                                                Status.completed
+                                            ? Icons.done
+                                            : Icons.arrow_forward_ios_sharp,
+                                        color:
+                                            kprimaryTextColor.withOpacity(0.2),
+                                      ),
+                                    )
+                                  ],
+                                ),
                               ),
                             ),
-                          ),
                           const SizedBox(
                             height: 20,
                             width: 0,
                           ),
-                          InkWell(
-                            onTap: () {
-                              if (controller.isMathsOngoing.value !=
-                                      Status.completed &&
-                                  controller.alarmRecord.isMathsEnabled) {
-                                controller.isMathsOngoing.value =
-                                    Status.ongoing;
-                                Get.to(() => MathsChallengeView());
-                              }
-                            },
-                            child: Container(
-                              width: width * 0.91,
-                              height: height * 0.1,
-                              decoration: const BoxDecoration(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(18)),
-                                color: ksecondaryBackgroundColor,
-                              ),
-                              child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceAround,
-                                children: [
-                                  Icon(
-                                    Icons.calculate_sharp,
-                                    color: kprimaryTextColor.withOpacity(0.8),
-                                    size: 28,
-                                  ),
-                                  Text(
-                                    'Maths Challenge',
-                                    style: Theme.of(context)
-                                        .textTheme
-                                        .bodyLarge!
-                                        .copyWith(
-                                          color: kprimaryTextColor,
-                                        ),
-                                  ),
-                                  Obx(
-                                    () => Icon(
-                                      (controller.alarmRecord.isMathsEnabled)
-                                          ? controller.isQrOngoing.value ==
-                                                  Status.completed
-                                              ? Icons.done
-                                              : Icons.arrow_forward_ios_sharp
-                                          : controller.isQrOngoing.value ==
-                                                  Status.completed
-                                              ? Icons.done
-                                              : Icons.close,
-                                      color: kprimaryTextColor.withOpacity(0.2),
+                          if (controller.alarmRecord.isMathsEnabled)
+                            InkWell(
+                              onTap: () {
+                                if (controller.isMathsOngoing.value !=
+                                        Status.completed &&
+                                    controller.alarmRecord.isMathsEnabled) {
+                                  controller.isMathsOngoing.value =
+                                      Status.ongoing;
+                                  Get.to(() => MathsChallengeView());
+                                }
+                              },
+                              child: Container(
+                                width: width * 0.91,
+                                height: height * 0.1,
+                                decoration: const BoxDecoration(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(18)),
+                                  color: ksecondaryBackgroundColor,
+                                ),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceAround,
+                                  children: [
+                                    Icon(
+                                      Icons.calculate_sharp,
+                                      color: kprimaryTextColor.withOpacity(0.8),
+                                      size: 28,
                                     ),
-                                  ),
-                                ],
+                                    Text(
+                                      'Maths Challenge',
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .bodyLarge!
+                                          .copyWith(
+                                            color: kprimaryTextColor,
+                                          ),
+                                    ),
+                                    Obx(
+                                      () => Icon(
+                                        controller.isQrOngoing.value ==
+                                                Status.completed
+                                            ? Icons.done
+                                            : Icons.arrow_forward_ios_sharp,
+                                        color:
+                                            kprimaryTextColor.withOpacity(0.2),
+                                      ),
+                                    ),
+                                  ],
+                                ),
                               ),
                             ),
-                          ),
                           const SizedBox(
                             height: 20,
                             width: 0,
                           ),
-                          InkWell(
-                            onTap: () {
-                              if (controller.isQrOngoing.value !=
-                                      Status.completed &&
-                                  controller.alarmRecord.isQrEnabled) {
-                                Get.to(() => QRChallengeView());
-                              }
-                            },
-                            child: Container(
-                              width: width * 0.91,
-                              height: height * 0.1,
-                              decoration: const BoxDecoration(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(18)),
-                                color: ksecondaryBackgroundColor,
-                              ),
-                              child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceAround,
-                                children: [
-                                  Icon(
-                                    Icons.qr_code_scanner,
-                                    color: kprimaryTextColor.withOpacity(0.8),
-                                    size: 28,
-                                  ),
-                                  Text(
-                                    'QR/Bar Code Challenge',
-                                    style: Theme.of(context)
-                                        .textTheme
-                                        .bodyLarge!
-                                        .copyWith(
-                                          color: kprimaryTextColor,
-                                        ),
-                                  ),
-                                  Obx(
-                                    () => Icon(
-                                      (controller.alarmRecord.isQrEnabled)
-                                          ? controller.isQrOngoing.value ==
-                                                  Status.completed
-                                              ? Icons.done
-                                              : Icons.arrow_forward_ios_sharp
-                                          : controller.isQrOngoing.value ==
-                                                  Status.completed
-                                              ? Icons.done
-                                              : Icons.close,
-                                      color: kprimaryTextColor.withOpacity(0.2),
+                          if (controller.alarmRecord.isQrEnabled)
+                            InkWell(
+                              onTap: () {
+                                if (controller.isQrOngoing.value !=
+                                        Status.completed &&
+                                    controller.alarmRecord.isQrEnabled) {
+                                  Get.to(() => QRChallengeView());
+                                }
+                              },
+                              child: Container(
+                                width: width * 0.91,
+                                height: height * 0.1,
+                                decoration: const BoxDecoration(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(18)),
+                                  color: ksecondaryBackgroundColor,
+                                ),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceAround,
+                                  children: [
+                                    Icon(
+                                      Icons.qr_code_scanner,
+                                      color: kprimaryTextColor.withOpacity(0.8),
+                                      size: 28,
                                     ),
-                                  ),
-                                ],
+                                    Text(
+                                      'QR/Bar Code Challenge',
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .bodyLarge!
+                                          .copyWith(
+                                            color: kprimaryTextColor,
+                                          ),
+                                    ),
+                                    Obx(
+                                      () => Icon(
+                                        controller.isQrOngoing.value ==
+                                                Status.completed
+                                            ? Icons.done
+                                            : Icons.arrow_forward_ios_sharp,
+                                        color:
+                                            kprimaryTextColor.withOpacity(0.2),
+                                      ),
+                                    ),
+                                  ],
+                                ),
                               ),
                             ),
-                          ),
                           const SizedBox(
                             height: 20,
                             width: 0,
