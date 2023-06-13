0e9887160d4ba45c5f14bb8ec0c47558e83db228
Rijuth Menon <rijuthm@gmail.com>
2023-06-13 21:54:48 +0530

    using put for home binding

--- lib/app/modules/home/bindings/home_binding.dart
+++ lib/app/modules/home/bindings/home_binding.dart
@@ -5,8 +5,8 @@ import '../controllers/home_controller.dart';
 class HomeBinding extends Bindings {
   @override
   void dependencies() {
-    Get.lazyPut<HomeController>(
-      () => HomeController(),
+    Get.put<HomeController>(
+      HomeController(),
     );
   }
 }
