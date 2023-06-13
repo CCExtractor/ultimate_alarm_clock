e713d60520ac7fcca19e8afd78a92db926646334
Rijuth Menon <rijuthm@gmail.com>
2023-06-13 06:54:23 +0530

    added modified firestore queries

--- lib/app/modules/alarmRing/controllers/alarm_ring_controller.dart
+++ lib/app/modules/alarmRing/controllers/alarm_ring_controller.dart
@@ -7,9 +7,11 @@ import 'package:flutter_ringtone_player/flutter_ringtone_player.dart';
 import 'package:get/get.dart';
 import 'package:ultimate_alarm_clock/app/data/models/alarm_handler_setup_model.dart';
 import 'package:ultimate_alarm_clock/app/data/models/alarm_model.dart';
+import 'package:ultimate_alarm_clock/app/data/models/user_model.dart';
 
 import 'package:ultimate_alarm_clock/app/data/providers/firestore_provider.dart';
 import 'package:ultimate_alarm_clock/app/data/providers/isar_provider.dart';
+import 'package:ultimate_alarm_clock/app/data/providers/secure_storage_provider.dart';
 import 'package:ultimate_alarm_clock/app/utils/utils.dart';
 
 class AlarmControlController extends GetxController
@@ -21,11 +23,12 @@ class AlarmControlController extends GetxController
       Utils.convertTo12HourFormat(Utils.timeOfDayToString(TimeOfDay.now())).obs;
 
   getCurrentlyRingingAlarm() async {
+    UserModel? _userModel = await SecureStorageProvider().retrieveUserModel();
     AlarmModel _alarmRecord = Utils.genFakeAlarmModel();
     AlarmModel isarLatestAlarm =
         await IsarDb.getLatestAlarm(_alarmRecord, false);
     AlarmModel firestoreLatestAlarm =
-        await FirestoreDb.getLatestAlarm(_alarmRecord, false);
+        await FirestoreDb.getLatestAlarm(_userModel, _alarmRecord, false);
     AlarmModel latestAlarm =
         Utils.getFirstScheduledAlarm(isarLatestAlarm, firestoreLatestAlarm);
     print("CURRENT RINGING : ${latestAlarm.alarmTime}");
@@ -33,11 +36,12 @@ class AlarmControlController extends GetxController
   }
 
   getNextAlarm() async {
+    UserModel? _userModel = await SecureStorageProvider().retrieveUserModel();
     AlarmModel _alarmRecord = Utils.genFakeAlarmModel();
     AlarmModel isarLatestAlarm =
         await IsarDb.getLatestAlarm(_alarmRecord, true);
     AlarmModel firestoreLatestAlarm =
-        await FirestoreDb.getLatestAlarm(_alarmRecord, true);
+        await FirestoreDb.getLatestAlarm(_userModel, _alarmRecord, true);
     AlarmModel latestAlarm =
         Utils.getFirstScheduledAlarm(isarLatestAlarm, firestoreLatestAlarm);
     print("LATEST : ${latestAlarm.alarmTime}");
