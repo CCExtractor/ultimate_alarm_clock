3ccb4cf6daf23fe88e04d280d38f22d12de69020
Rijuth Menon <rijuthm@gmail.com>
2023-06-15 21:58:47 +0530

    binding home controller to settings

--- lib/app/modules/settings/bindings/settings_binding.dart
+++ lib/app/modules/settings/bindings/settings_binding.dart
@@ -1,4 +1,5 @@
 import 'package:get/get.dart';
+import 'package:ultimate_alarm_clock/app/modules/home/controllers/home_controller.dart';
 
 import '../controllers/settings_controller.dart';
 
@@ -7,6 +8,9 @@ class SettingsBinding extends Bindings {
   void dependencies() {
     Get.lazyPut<SettingsController>(
       () => SettingsController(),
+    );
+    Get.put<HomeController>(
+      HomeController(),
     );
   }
 }
