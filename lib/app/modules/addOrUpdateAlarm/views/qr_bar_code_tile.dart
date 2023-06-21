fc39d22f6fcdfdd9b9015393fa023f2d3ce1bbb4
Rijuth Menon <rijuthm@gmail.com>
2023-06-21 17:47:32 +0530

    added info for QR challenge

--- lib/app/modules/addOrUpdateAlarm/views/qr_bar_code_tile.dart
+++ lib/app/modules/addOrUpdateAlarm/views/qr_bar_code_tile.dart
@@ -14,8 +14,74 @@ class QrBarCode extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var height = Get.height;
+    var width = Get.width;
     return ListTile(
-      title: const Text('QR/Bar Code'),
+      title: Row(
+        children: [
+          const Text('QR/Bar Code'),
+          IconButton(
+            icon: Icon(
+              Icons.info_sharp,
+              size: 21,
+              color: kprimaryTextColor.withOpacity(0.3),
+            ),
+            onPressed: () {
+              showModalBottomSheet(
+                  context: context,
+                  backgroundColor: ksecondaryBackgroundColor,
+                  builder: (context) {
+                    return Center(
+                      child: Padding(
+                        padding: const EdgeInsets.all(25.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Icon(
+                              Icons.qr_code_scanner,
+                              color: kprimaryTextColor,
+                              size: height * 0.1,
+                            ),
+                            Text("QR / Bar code",
+                                textAlign: TextAlign.center,
+                                style:
+                                    Theme.of(context).textTheme.displayMedium),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 15.0),
+                              child: Text(
+                                "Scan the QR/Bar code on any object, like a book, and relocate it to a different room. To deactivate the alarm, simply rescan the same QR/Bar code.",
+                                style: Theme.of(context).textTheme.bodyMedium,
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
+                            SizedBox(
+                              width: width,
+                              child: TextButton(
+                                style: ButtonStyle(
+                                  backgroundColor:
+                                      MaterialStateProperty.all(kprimaryColor),
+                                ),
+                                onPressed: () {
+                                  Get.back();
+                                },
+                                child: Text(
+                                  'Understood',
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .displaySmall!
+                                      .copyWith(color: ksecondaryTextColor),
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    );
+                  });
+            },
+          ),
+        ],
+      ),
       onTap: () {
         controller.restartQRCodeController();
         Get.defaultDialog(
