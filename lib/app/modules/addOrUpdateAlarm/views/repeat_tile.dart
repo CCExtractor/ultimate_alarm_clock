2d1b355eaceba66f66eb2dcf28dabf2a20cdc659
BrawlerXull <111864031+BrawlerXull@users.noreply.github.com>
2023-06-24 00:06:33 +0530

    Done button added
    In old ui the user have to click anywhere outside the area but now I've added a done button which closed the dialogue box

--- lib/app/modules/addOrUpdateAlarm/views/repeat_tile.dart
+++ lib/app/modules/addOrUpdateAlarm/views/repeat_tile.dart
@@ -226,6 +226,33 @@ class RepeatTile extends StatelessWidget {
                           ]),
                     ),
                   ),
+                  InkWell(
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 10.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {
+                              Get.back();
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.black45, // Set the desired background color
+                            ),
+                            child: Text(
+                              'Done',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .bodySmall!
+                                  .copyWith(fontSize: 15),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+
                 ],
               ),
             ));
