a8e2c503996a2bee3ce7cceecb04b5270872c1d9
BrawlerXull <111864031+BrawlerXull@users.noreply.github.com>
2023-08-27 19:14:30 +0530

    you can't go back while on rining in screen (#37)

--- lib/app/modules/alarmRing/views/alarm_ring_view.dart
+++ lib/app/modules/alarmRing/views/alarm_ring_view.dart
@@ -14,121 +14,130 @@ class AlarmControlView extends GetView<AlarmControlController> {
   Widget build(BuildContext context) {
     var width = Get.width;
     var height = Get.height;
-    return SafeArea(
-      child: Scaffold(
-          floatingActionButtonLocation:
-              FloatingActionButtonLocation.centerDocked,
-          floatingActionButton: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Padding(
-                  padding: EdgeInsets.all(18.0),
-                  child: Obx(
-                    () => SizedBox(
-                      height: height * 0.06,
-                      width: width * 0.8,
-                      child: controller.showButton.value
-                          ? TextButton(
-                              style: ButtonStyle(
-                                  backgroundColor:
-                                      MaterialStateProperty.all(kprimaryColor)),
-                              child: Text(
-                                Utils.isChallengeEnabled(
-                                        controller.currentlyRingingAlarm.value)
-                                    ? 'Start Challenge'
-                                    : 'Dismiss',
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .displaySmall!
-                                    .copyWith(color: ksecondaryTextColor),
-                              ),
-                              onPressed: () {
-                                if (Utils.isChallengeEnabled(
-                                    controller.currentlyRingingAlarm.value)) {
-                                  Get.toNamed('/alarm-challenge',
-                                      arguments: controller
-                                          .currentlyRingingAlarm.value);
-                                } else {
-                                  Get.offNamed('/home');
-                                }
-                              },
-                            )
-                          : SizedBox(),
-                    ),
-                  )),
-              (Get.arguments != null)
-                  ? Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: SizedBox(
+    return WillPopScope(
+        child: SafeArea(
+          child: Scaffold(
+              floatingActionButtonLocation:
+                  FloatingActionButtonLocation.centerDocked,
+              floatingActionButton: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Padding(
+                      padding: EdgeInsets.all(18.0),
+                      child: Obx(
+                        () => SizedBox(
                           height: height * 0.06,
-                          width: width,
-                          child: TextButton(
-                            style: ButtonStyle(
-                                backgroundColor: MaterialStateProperty.all(
-                                    kprimaryTextColor.withOpacity(0.7))),
-                            child: Text(
-                              'Exit Preview',
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .displaySmall!
-                                  .copyWith(color: ksecondaryTextColor),
-                            ),
-                            onPressed: () {
-                              Get.offNamed('/home');
-                            },
-                          )),
-                    )
-                  : SizedBox()
-            ],
-          ),
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Obx(
-                  () => Column(
-                    children: [
-                      Text(
-                        controller.formattedDate.value,
-                        style: Theme.of(context).textTheme.bodyLarge,
+                          width: width * 0.8,
+                          child: controller.showButton.value
+                              ? TextButton(
+                                  style: ButtonStyle(
+                                      backgroundColor:
+                                          MaterialStateProperty.all(
+                                              kprimaryColor)),
+                                  child: Text(
+                                    Utils.isChallengeEnabled(controller
+                                            .currentlyRingingAlarm.value)
+                                        ? 'Start Challenge'
+                                        : 'Dismiss',
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .displaySmall!
+                                        .copyWith(color: ksecondaryTextColor),
+                                  ),
+                                  onPressed: () {
+                                    if (Utils.isChallengeEnabled(controller
+                                        .currentlyRingingAlarm.value)) {
+                                      Get.toNamed('/alarm-challenge',
+                                          arguments: controller
+                                              .currentlyRingingAlarm.value);
+                                    } else {
+                                      Get.offNamed('/home');
+                                    }
+                                  },
+                                )
+                              : SizedBox(),
+                        ),
+                      )),
+                  (Get.arguments != null)
+                      ? Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: SizedBox(
+                              height: height * 0.06,
+                              width: width,
+                              child: TextButton(
+                                style: ButtonStyle(
+                                    backgroundColor: MaterialStateProperty.all(
+                                        kprimaryTextColor.withOpacity(0.7))),
+                                child: Text(
+                                  'Exit Preview',
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .displaySmall!
+                                      .copyWith(color: ksecondaryTextColor),
+                                ),
+                                onPressed: () {
+                                  Get.offNamed('/home');
+                                },
+                              )),
+                        )
+                      : SizedBox()
+                ],
+              ),
+              body: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Obx(
+                      () => Column(
+                        children: [
+                          Text(
+                            controller.formattedDate.value,
+                            style: Theme.of(context).textTheme.bodyLarge,
+                          ),
+                          const SizedBox(
+                            height: 10,
+                            width: 0,
+                          ),
+                          Text(
+                            (controller.isSnoozing.value)
+                                ? "${controller.minutes.toString().padLeft(2, '0')}:${controller.seconds.toString().padLeft(2, '0')}"
+                                : "${controller.timeNow[0]} ${controller.timeNow[1]}",
+                            style: Theme.of(context)
+                                .textTheme
+                                .displayLarge!
+                                .copyWith(fontSize: 50),
+                          ),
+                        ],
                       ),
-                      const SizedBox(
-                        height: 10,
-                        width: 0,
-                      ),
-                      Text(
-                        (controller.isSnoozing.value)
-                            ? "${controller.minutes.toString().padLeft(2, '0')}:${controller.seconds.toString().padLeft(2, '0')}"
-                            : "${controller.timeNow[0]} ${controller.timeNow[1]}",
-                        style: Theme.of(context)
-                            .textTheme
-                            .displayLarge!
-                            .copyWith(fontSize: 50),
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  height: height * 0.055,
-                  width: width * 0.25,
-                  child: TextButton(
-                    style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all(
-                            ksecondaryBackgroundColor)),
-                    child: Text(
-                      'Snooze',
-                      style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                          color: kprimaryTextColor,
-                          fontWeight: FontWeight.w600),
                     ),
-                    onPressed: () {
-                      controller.startSnooze();
-                    },
-                  ),
-                )
-              ],
-            ),
-          )),
-    );
+                    SizedBox(
+                      height: height * 0.055,
+                      width: width * 0.25,
+                      child: TextButton(
+                        style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all(
+                                ksecondaryBackgroundColor)),
+                        child: Text(
+                          'Snooze',
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodyMedium!
+                              .copyWith(
+                                  color: kprimaryTextColor,
+                                  fontWeight: FontWeight.w600),
+                        ),
+                        onPressed: () {
+                          controller.startSnooze();
+                        },
+                      ),
+                    )
+                  ],
+                ),
+              )),
+        ),
+        onWillPop: () async {
+          Get.snackbar("Note", "You can't go back while the alarm is ringing");
+          return false;
+        });
   }
 }
