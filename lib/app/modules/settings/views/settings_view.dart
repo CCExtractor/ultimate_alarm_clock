54e38777f6fb525457f3b26531753fff059fc072
BrawlerXull <111864031+BrawlerXull@users.noreply.github.com>
2023-09-24 15:21:50 +0530

    haptic bug (#73)

--- lib/app/modules/settings/views/settings_view.dart
+++ lib/app/modules/settings/views/settings_view.dart
@@ -6,6 +6,7 @@ import 'package:ultimate_alarm_clock/app/modules/home/controllers/home_controlle
 import 'package:ultimate_alarm_clock/app/modules/settings/views/enable_haptic_feedback.dart';
 import 'package:ultimate_alarm_clock/app/modules/settings/views/enable_sorted_alarm_list.dart';
 import 'package:ultimate_alarm_clock/app/modules/settings/views/weather_api.dart';
+import 'package:ultimate_alarm_clock/app/utils/utils.dart';
 import '../controllers/settings_controller.dart';
 import 'google_sign_in.dart';
 
@@ -23,6 +24,13 @@ class SettingsView extends GetView<SettingsController> {
           title: const Text('Settings'),
           centerTitle: true,
           elevation: 0.0,
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () {
+              Utils.hapticFeedback();
+              Navigator.of(context).pop();
+            },
+          ),
         ),
         body: Center(
           child: Padding(
