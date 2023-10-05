1087e0a2f2f41a23ce9f14e1d4a63f6cc59a01c2
tavkeeer <105163810+tavkeer@users.noreply.github.com>
2023-10-05 13:45:39 +0530

    #94 : Proposed the change in the status bar colour for better UI (#95)
    * update
    
    * fixed status bar color problem
    
    * Feat: fixed status bar colors for light and dark modes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/services.dart';
-
 import 'package:get/get.dart';
 import 'package:ultimate_alarm_clock/app/utils/constants.dart';
 import 'app/routes/app_pages.dart';
@@ -15,6 +14,9 @@ void main() async {
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
   ]);
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(statusBarColor: Colors.transparent),
+  );
 
   runApp(
     GetMaterialApp(
