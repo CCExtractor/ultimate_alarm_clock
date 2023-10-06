b8535cb63683c72b886f260fc128dc122e83a227
superiorsd10 <sachindapkara6@gmail.com>
2023-10-06 14:21:27 +0530

    adding comments to the secure_storage_provider

--- lib/app/data/providers/secure_storage_provider.dart
+++ lib/app/data/providers/secure_storage_provider.dart
@@ -48,6 +48,7 @@ class SecureStorageProvider {
     return await _secureStorage.read(key: apiKey);
   }
 
+  // Store the weather state in the flutter secure storage
   Future<void> storeWeatherState(String weatherState) async {
     const String key = 'weather_state';
     await _secureStorage.write(
@@ -56,6 +57,7 @@ class SecureStorageProvider {
     );
   }
 
+  // Get the weather state from the flutter secure storage
   Future<String?> retrieveWeatherState() async {
     const String key = 'weather_state';
     return await _secureStorage.read(key: key);
