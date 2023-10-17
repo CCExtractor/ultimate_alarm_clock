bb331105f73b32bd4a4f276dbee324b65ffc2f29
superiorsd10 <sachindapkara6@gmail.com>
2023-10-17 13:44:56 +0530

    resolving merge conflicts

--- lib/app/modules/settings/views/weather_api.dart
+++ lib/app/modules/settings/views/weather_api.dart
@@ -37,7 +37,9 @@ class WeatherApi extends StatelessWidget {
               alignment: Alignment.center,
               children: [
                 Container(
-                  // If the user hasn't clicked on the 'Save' or 'Update' button of the dialog, or there is no error in the weather key, then show the dialog
+                  // If the user hasn't clicked on the 'Save' or 'Update' button of
+                  // the dialog, or there is no error in the weather key, then
+                  // show the dialog
                   child: (controller.weatherKeyState.value !=
                               WeatherKeyState.saveAdded &&
                           controller.weatherKeyState.value !=
@@ -56,94 +58,104 @@ class WeatherApi extends StatelessWidget {
                                         MainAxisAlignment.spaceEvenly,
                                     children: [
                                       TextButton(
-                                          style: ButtonStyle(
-                                            backgroundColor:
-                                                MaterialStateProperty.all(
-                                                    kprimaryColor),
+                                        style: ButtonStyle(
+                                          backgroundColor:
+                                              MaterialStateProperty.all(
+                                            kprimaryColor,
                                           ),
-                                          child: Text(
-                                            // If the weather state is add, then show 'Save' else show 'Update' text on the button
-                                            controller.weatherKeyState.value ==
-                                                    WeatherKeyState.add
-                                                ? 'Save'
-                                                : 'Update',
-                                            style: Theme.of(context)
-                                                .textTheme
-                                                .displaySmall!
-                                                .copyWith(
-                                                    color: themeController
-                                                            .isLightMode.value
-                                                        ? kLightPrimaryTextColor
-                                                        : ksecondaryTextColor),
-                                          ),
-                                          onPressed: () async {
-                                            Utils.hapticFeedback();
-                                            controller.showingCircularProgressIndicator.value = true;
+                                        ),
+                                        child: Text(
+                                          // If the weather state is add, then
+                                          // show 'Save' else show 'Update' text
+                                          // on the button
+                                          controller.weatherKeyState.value ==
+                                                  WeatherKeyState.add
+                                              ? 'Save'
+                                              : 'Update',
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .displaySmall!
+                                              .copyWith(
+                                                color: themeController
+                                                        .isLightMode.value
+                                                    ? kLightPrimaryTextColor
+                                                    : ksecondaryTextColor,
+                                              ),
+                                        ),
+                                        onPressed: () async {
+                                          Utils.hapticFeedback();
+                                          controller
+                                              .showingCircularProgressIndicator
+                                              .value = true;
 
-                                            // Get the user's location
-                                            await controller.getLocation();
+                                          // Get the user's location
+                                          await controller.getLocation();
 
-                                            // If the API key is valid
-                                            if (await controller.isApiKeyValid(
-                                                controller.apiKey.text)) {
-                                              // Add the key to the storage
-                                              await controller.addKey(
-                                                  ApiKeys.openWeatherMap,
-                                                  controller.apiKey.text);
+                                          // If the API key is valid
+                                          if (await controller.isApiKeyValid(
+                                            controller.apiKey.text,
+                                          )) {
+                                            // Add the key to the storage
+                                            await controller.addKey(
+                                              ApiKeys.openWeatherMap,
+                                              controller.apiKey.text,
+                                            );
 
-                                              // If it is added for the first time
-                                              if (controller
-                                                      .weatherKeyState.value ==
-                                                  WeatherKeyState.add) {
-                                                controller
-                                                        .weatherKeyState.value =
-                                                    WeatherKeyState.saveAdded;
-                                                controller.addWeatherState(
-                                                    'saveAdded');
-                                              } else {
-                                                // If it is updated
-                                                controller
-                                                        .weatherKeyState.value =
-                                                    WeatherKeyState.saveUpdated;
-                                                controller.addWeatherState(
-                                                    'saveUpdated');
-                                              }
+                                            // If it is added for the first time
+                                            if (controller
+                                                    .weatherKeyState.value ==
+                                                WeatherKeyState.add) {
+                                              controller.weatherKeyState.value =
+                                                  WeatherKeyState.saveAdded;
+                                              controller
+                                                  .addWeatherState('saveAdded');
                                             } else {
-                                              // If the API key is not valid
-                                              controller.didWeatherKeyError
-                                                  .value = true;
+                                              // If it is updated
+                                              controller.weatherKeyState.value =
+                                                  WeatherKeyState.saveUpdated;
+                                              controller.addWeatherState(
+                                                  'saveUpdated');
                                             }
-                                            controller.showingCircularProgressIndicator.value = false;
-                                          }),
+                                          } else {
+                                            // If the API key is not valid
+                                            controller.didWeatherKeyError
+                                                .value = true;
+                                          }
+                                          controller
+                                              .showingCircularProgressIndicator
+                                              .value = false;
+                                        },
+                                      ),
                                       OutlinedButton(
-                                          style: OutlinedButton.styleFrom(
-                                            side: const BorderSide(
-                                              color: kprimaryColor,
-                                              width: 1,
-                                            ),
+                                        style: OutlinedButton.styleFrom(
+                                          side: const BorderSide(
+                                            color: kprimaryColor,
+                                            width: 1,
                                           ),
-                                          child: Text(
-                                            'Get Key',
-                                            style: Theme.of(context)
-                                                .textTheme
-                                                .displaySmall!
-                                                .copyWith(
-                                                  color: kprimaryColor,
-                                                ),
-                                          ),
-                                          onPressed: () async {
-                                            Utils.hapticFeedback();
-                                            const url =
-                                                'https://home.openweathermap.org/api_keys';
-                                            if (await canLaunchUrlString(url)) {
-                                              await launchUrlString(url);
-                                            } else {
-                                              throw 'Could not launch $url';
-                                            }
-                                          }),
+                                        ),
+                                        child: Text(
+                                          'Get Key',
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .displaySmall!
+                                              .copyWith(
+                                                color: kprimaryColor,
+                                              ),
+                                        ),
+                                        onPressed: () async {
+                                          Utils.hapticFeedback();
+                                          const url =
+                                              'https://home.openweathermap.org/api_keys';
+                                          if (await canLaunchUrlString(url)) {
+                                            await launchUrlString(url);
+                                          } else {
+                                            throw 'Could not launch $url';
+                                          }
+                                        },
+                                      ),
                                     ],
                                   ),
-                                )
+                                ),
                               ],
                             )
                           : Column(
@@ -164,26 +176,28 @@ class WeatherApi extends StatelessWidget {
                                   ),
                                 ),
                                 TextButton(
-                                    style: ButtonStyle(
-                                        backgroundColor:
-                                            MaterialStateProperty.all(
-                                                kprimaryColor)),
-                                    child: Text(
-                                      'Retry',
-                                      style: Theme.of(context)
-                                          .textTheme
-                                          .displaySmall!
-                                          .copyWith(
-                                              color: themeController
-                                                      .isLightMode.value
-                                                  ? kLightSecondaryTextColor
-                                                  : ksecondaryTextColor),
+                                  style: ButtonStyle(
+                                    backgroundColor: MaterialStateProperty.all(
+                                      kprimaryColor,
                                     ),
-                                    onPressed: () {
-                                      Utils.hapticFeedback();
-                                      controller.didWeatherKeyError.value =
-                                          false;
-                                    }),
+                                  ),
+                                  child: Text(
+                                    'Retry',
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .displaySmall!
+                                        .copyWith(
+                                          color:
+                                              themeController.isLightMode.value
+                                                  ? kLightSecondaryTextColor
+                                                  : ksecondaryTextColor,
+                                        ),
+                                  ),
+                                  onPressed: () {
+                                    Utils.hapticFeedback();
+                                    controller.didWeatherKeyError.value = false;
+                                  },
+                                ),
                               ],
                             )
                       : Column(
@@ -201,34 +215,38 @@ class WeatherApi extends StatelessWidget {
                                 controller.weatherKeyState.value ==
                                         WeatherKeyState.saveAdded
                                     ? 'Your API Key is added!' // show this message
-                                    : 'Your API Key is updated!', // else this message
+                                    : 'Your API Key is updated!',
+                                // else this message
                                 style: Theme.of(context).textTheme.displaySmall,
                               ),
                             ),
                             TextButton(
-                                style: ButtonStyle(
-                                    backgroundColor: MaterialStateProperty.all(
-                                        kprimaryColor)),
-                                child: Text(
-                                  'Okay',
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .displaySmall!
-                                      .copyWith(
-                                          color:
-                                              themeController.isLightMode.value
-                                                  ? kLightSecondaryTextColor
-                                                  : ksecondaryTextColor),
-                                ),
-                                onPressed: () {
-                                  Utils.hapticFeedback();
+                              style: ButtonStyle(
+                                backgroundColor:
+                                    MaterialStateProperty.all(kprimaryColor),
+                              ),
+                              child: Text(
+                                'Okay',
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .displaySmall!
+                                    .copyWith(
+                                      color: themeController.isLightMode.value
+                                          ? kLightSecondaryTextColor
+                                          : ksecondaryTextColor,
+                                    ),
+                              ),
+                              onPressed: () {
+                                Utils.hapticFeedback();
 
-                                  // Assign the weather state to update, and save it in the local storage
-                                  controller.weatherKeyState.value =
-                                      WeatherKeyState.update;
-                                  controller.addWeatherState('update');
-                                  Get.back();
-                                }),
+                                // Assign the weather state to update, and save
+                                // it in the local storage
+                                controller.weatherKeyState.value =
+                                    WeatherKeyState.update;
+                                controller.addWeatherState('update');
+                                Get.back();
+                              },
+                            ),
                           ],
                         ),
                 ),
@@ -250,12 +268,15 @@ class WeatherApi extends StatelessWidget {
         width: width * 0.91,
         height: height * 0.1,
         decoration: Utils.getCustomTileBoxDecoration(
-            isLightMode: themeController.isLightMode.value),
+          isLightMode: themeController.isLightMode.value,
+        ),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
-            Text('Open Weather Map API',
-                style: Theme.of(context).textTheme.bodyLarge),
+            Text(
+              'Open Weather Map API',
+              style: Theme.of(context).textTheme.bodyLarge,
+            ),
             Icon(
               Icons.arrow_forward_ios_sharp,
               color: themeController.isLightMode.value
