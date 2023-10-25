f2ec2cafa71190a4019e729d42ba4e0fc876ba31
Sameer Mansoori <sameernk220@gmail.com>
2023-10-25 21:26:39 +0530

    Exit preview button (color and text) Now working properly according to whatever was defined in ThemeController

--- lib/app/modules/alarmRing/views/alarm_ring_view.dart
+++ lib/app/modules/alarmRing/views/alarm_ring_view.dart
@@ -74,32 +74,34 @@ class AlarmControlView extends GetView<AlarmControlController> {
               (Get.arguments != null)
                   ? Padding(
                       padding: const EdgeInsets.all(8.0),
-                      child: SizedBox(
-                        height: height * 0.06,
-                        width: width,
-                        child: TextButton(
-                          style: ButtonStyle(
-                            backgroundColor: MaterialStateProperty.all(
-                              themeController.isLightMode.value
-                                  ? kLightPrimaryTextColor.withOpacity(0.7)
-                                  : kprimaryTextColor.withOpacity(0.7),
+                      child: Obx(
+                        () => SizedBox(
+                          height: height * 0.06,
+                          width: width,
+                          child: TextButton(
+                            style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all(
+                                themeController.isLightMode.value
+                                    ? kLightPrimaryTextColor.withOpacity(0.7)
+                                    : kprimaryTextColor.withOpacity(0.7),
+                              ),
                             ),
+                            child: Text(
+                              'Exit Preview',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .displaySmall!
+                                  .copyWith(
+                                    color: themeController.isLightMode.value
+                                        ? kLightPrimaryTextColor
+                                        : ksecondaryTextColor,
+                                  ),
+                            ),
+                            onPressed: () {
+                              Utils.hapticFeedback();
+                              Get.offNamed('/home');
+                            },
                           ),
-                          child: Text(
-                            'Exit Preview',
-                            style: Theme.of(context)
-                                .textTheme
-                                .displaySmall!
-                                .copyWith(
-                                  color: themeController.isLightMode.value
-                                      ? kLightPrimaryTextColor
-                                      : ksecondaryTextColor,
-                                ),
-                          ),
-                          onPressed: () {
-                            Utils.hapticFeedback();
-                            Get.offNamed('/home');
-                          },
                         ),
                       ),
                     )
