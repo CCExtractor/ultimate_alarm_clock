03d7758a69422c179d2c54ce84bcb66a1bea4c08
superiorsd10 <sachindapkara6@gmail.com>
2023-12-09 22:46:52 +0530

    replacing the deprecated methods

--- lib/app/modules/about/controller/about_controller.dart
+++ lib/app/modules/about/controller/about_controller.dart
@@ -1,16 +1,17 @@
 import 'package:get/get.dart';
 import 'package:ultimate_alarm_clock/app/modules/about/views/about_view.dart';
-import 'package:url_launcher/url_launcher.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 class AboutController extends GetxController {
   final RxBool isAboutExpanded = true.obs;
 
-  static const String ccExtractorUrl = "https://ccextractor.org/";
-  static const String githubUrl = "https://github.com/CCExtractor/ultimate_alarm_clock";
+  static const String ccExtractorUrl = 'https://ccextractor.org/';
+  static const String githubUrl =
+      'https://github.com/CCExtractor/ultimate_alarm_clock';
 
   Future<bool> launchUrl(Uri uri) async {
-    if (await canLaunch(uri.toString())) {
-      return await launch(uri.toString());
+    if (await canLaunchUrlString(uri.toString())) {
+      return await launchUrlString(uri.toString());
     } else {
       return false;
     }
