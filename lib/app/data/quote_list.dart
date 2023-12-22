032cdb06b34428df9cc37d0a26d69525ef36897a
superiorsd10 <sachindapkara6@gmail.com>
2023-12-22 14:14:55 +0530

    adding the quote list in the data folder

---
+++ lib/app/data/quote_list.dart
@@ -0,0 +1,269 @@
+// ignore_for_file: lines_longer_than_80_chars
+
+const List quoteList = [
+  {
+    'quote':
+        'Whatever the mind of man can conceive and believe, it can achieve.',
+    'author': 'Napoleon Hill',
+  },
+  {
+    'quote': 'Strive not to be a success, but rather to be of value.',
+    'author': 'Albert Einstein',
+  },
+  {
+    'quote': 'I attribute my success to this: I never gave or took any excuse.',
+    'author': 'Florence Nightingale',
+  },
+  {
+    'quote': "You miss 100% of the shots you don't take.",
+    'author': 'Wayne Gretzky',
+  },
+  {
+    'quote':
+        'The most difficult thing is the decision to act, the rest is merely tenacity.',
+    'author': 'Amelia Earhart',
+  },
+  {
+    'quote': 'Every strike brings me closer to the next home run.',
+    'author': 'Babe Ruth',
+  },
+  {
+    'quote':
+        'Definiteness of purpose is the starting point of all achievement.',
+    'author': 'W. Clement Stone',
+  },
+  {
+    'quote':
+        "Life isn't about getting and having, it's about giving and being.",
+    'author': 'Kevin Kruse',
+  },
+  {
+    'quote':
+        "Life is what happens to you while you're busy making other plans.",
+    'author': 'John Lennon',
+  },
+  {'quote': 'We become what we think about.', 'author': 'Earl Nightingale'},
+  {
+    'quote': 'Life is 10% what happens to me and 90% of how I react to it.',
+    'author': 'Charles Swindoll',
+  },
+  {
+    'quote':
+        "The most common way people give up their power is by thinking they don't have any.",
+    'author': 'Alice Walker',
+  },
+  {
+    'quote': 'The mind is everything. What you think you become.',
+    'author': 'Buddha',
+  },
+  {
+    'quote':
+        'The best time to plant a tree was 20 years ago. The second best time is now.',
+    'author': 'Chinese Proverb',
+  },
+  {'quote': 'An unexamined life is not worth living.', 'author': 'Socrates'},
+  {
+    'quote': 'Eighty percent of success is showing up.',
+    'author': 'Woody Allen',
+  },
+  {
+    'quote':
+        "Your time is limited, so don't waste it living someone else's life.",
+    'author': 'Steve Jobs',
+  },
+  {
+    'quote': 'Winning isn\'t everything, but wanting to win is.',
+    'author': 'Vince Lombardi',
+  },
+  {
+    'quote':
+        'I am not a product of my circumstances. I am a product of my decisions.',
+    'author': 'Stephen Covey',
+  },
+  {
+    'quote':
+        'Every child is an artist.  The problem is how to remain an artist once he grows up.',
+    'author': 'Pablo Picasso',
+  },
+  {
+    'quote':
+        'You can never cross the ocean until you have the courage to lose sight of the shore.',
+    'author': 'Christopher Columbus',
+  },
+  {
+    'quote': 'Either you run the day, or the day runs you.',
+    'author': 'Jim Rohn',
+  },
+  {
+    'quote': "Whether you think you can or you think you can't, you're right.",
+    'author': 'Henry Ford',
+  },
+  {'quote': 'The best revenge is massive success.', 'author': 'Frank Sinatra'},
+  {
+    'quote': "Life shrinks or expands in proportion to one's courage.",
+    'author': 'Anais Nin',
+  },
+  {
+    'quote':
+        'There is only one way to avoid criticism: do nothing, say nothing, and be nothing.',
+    'author': 'Aristotle',
+  },
+  {
+    'quote':
+        'Certain things catch your eye, but pursue only those that capture the heart.',
+    'author': 'Ancient Indian Proverb',
+  },
+  {
+    'quote': "Believe you can and you're halfway there.",
+    'author': 'Theodore Roosevelt',
+  },
+  {
+    'quote': "Everything you've ever wanted is on the other side of fear.",
+    'author': 'George Addair',
+  },
+  {
+    'quote': 'Start where you are. Use what you have.  Do what you can.',
+    'author': 'Arthur Ashe',
+  },
+  {
+    'quote': 'Fall seven times and stand up eight.',
+    'author': 'Japanese Proverb',
+  },
+  {
+    'quote': 'Everything has beauty, but not everyone can see.',
+    'author': 'Confucius',
+  },
+  {
+    'quote': 'When I let go of what I am, I become what I might be.',
+    'author': 'Lao Tzu',
+  },
+  {
+    'quote':
+        'Happiness is not something readymade.  It comes from your own actions.',
+    'author': 'Dalai Lama',
+  },
+  {
+    'quote': 'If the wind will not serve, take to the oars.',
+    'author': 'Latin Proverb',
+  },
+  {
+    'quote':
+        'Too many of us are not living our dreams because we are living our fears.',
+    'author': 'Les Brown',
+  },
+  {
+    'quote': 'If you want to lift yourself up, lift up someone else.',
+    'author': 'Booker T. Washington',
+  },
+  {
+    'quote': "I didn't fail the test. I just found 100 ways to do it wrong.",
+    'author': 'Benjamin Franklin',
+  },
+  {
+    'quote':
+        'In order to succeed, your desire for success should be greater than your fear of failure.',
+    'author': 'Bill Cosby',
+  },
+  {
+    'quote': 'There are no traffic jams along the extra mile.',
+    'author': 'Roger Staubach',
+  },
+  {
+    'quote': 'It is never too late to be what you might have been.',
+    'author': 'George Eliot',
+  },
+  {'quote': 'You become what you believe.', 'author': 'Oprah Winfrey'},
+  {
+    'quote': 'I would rather die of passion than of boredom.',
+    'author': 'Vincent van Gogh',
+  },
+  {
+    'quote':
+        'A truly rich man is one whose children run into his arms when his hands are empty.',
+    'author': 'Unknown',
+  },
+  {
+    'quote':
+        'Build your own dreams, or someone else will hire you to build theirs.',
+    'author': 'Farrah Gray',
+  },
+  {
+    'quote': 'Education costs money.  But then so does ignorance.',
+    'author': 'Sir Claus Moser',
+  },
+  {
+    'quote':
+        "I have learned over the years that when one's mind is made up, this diminishes fear.",
+    'author': 'Rosa Parks',
+  },
+  {
+    'quote': 'It does not matter how slowly you go as long as you do not stop.',
+    'author': 'Confucius',
+  },
+  {
+    'quote':
+        'Remember that not getting what you want is sometimes a wonderful stroke of luck.',
+    'author': 'Dalai Lama',
+  },
+  {
+    'quote':
+        "You can't use up creativity.  The more you use, the more you have.",
+    'author': 'Maya Angelou',
+  },
+  {'quote': 'Dream big and dare to fail.', 'author': 'Norman Vaughan'},
+  {
+    'quote':
+        'Our lives begin to end the day we become silent about things that matter.',
+    'author': 'Martin Luther King Jr.',
+  },
+  {
+    'quote': 'Do what you can, where you are, with what you have.',
+    'author': 'Teddy Roosevelt',
+  },
+  {
+    'quote':
+        "If you do what you've always done, you'll get what you've always gotten.",
+    'author': 'Tony Robbins',
+  },
+  {
+    'quote': 'Dreaming, after all, is a form of planning.',
+    'author': 'Gloria Steinem',
+  },
+  {
+    'quote':
+        "You may be disappointed if you fail, but you are doomed if you don't try.",
+    'author': 'Beverly Sills',
+  },
+  {
+    'quote': 'Remember no one can make you feel inferior without your consent.',
+    'author': 'Eleanor Roosevelt',
+  },
+  {
+    'quote': 'Life is what we make it, always has been, always will be.',
+    'author': 'Grandma Moses',
+  },
+  {
+    'quote':
+        "The question isn't who is going to let me; it's who is going to stop me.",
+    'author': 'Ayn Rand',
+  },
+  {
+    'quote':
+        "It's not the years in your life that count. It's the life in your years.",
+    'author': 'Abraham Lincoln',
+  },
+  {
+    'quote': 'Change your thoughts and you change your world.',
+    'author': 'Norman Vincent Peale',
+  },
+  {
+    'quote':
+        'Either write something worth reading or do something worth writing.',
+    'author': 'Benjamin Franklin',
+  },
+  {
+    'quote': 'The only way to do great work is to love what you do.',
+    'author': 'Steve Jobs',
+  },
+  {'quote': 'If you can dream it, you can achieve it.', 'author': 'Zig Ziglar'},
+];
