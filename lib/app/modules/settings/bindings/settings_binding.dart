745e11e445110ef48dc4758f54152c3647d5a55c
superiorsd10 <sachindapkara6@gmail.com>
2023-12-29 00:28:56 +0530

    adding the timer and bottom-nav-bar controllers to settings-binding

--- lib/app/modules/settings/bindings/settings_binding.dart
+++ lib/app/modules/settings/bindings/settings_binding.dart
@@ -1,15 +1,25 @@
 import 'package:get/get.dart';
+import 'package:ultimate_alarm_clock/app/modules/bottomNavigationBar/controllers/bottom_navigation_bar_controller.dart';
 import 'package:ultimate_alarm_clock/app/modules/home/controllers/home_controller.dart';
-import 'package:ultimate_alarm_clock/app/modules/settings/controllers/theme_controller.dart';
+import 'package:ultimate_alarm_clock/app/modules/timer/controllers/timer_controller.dart';
 
 import '../controllers/settings_controller.dart';
 
 class SettingsBinding extends Bindings {
   @override
   void dependencies() {
-    Get.lazyPut<SettingsController>(() => SettingsController(), fenix: true);
+    Get.lazyPut<SettingsController>(
+      () => SettingsController(),
+      fenix: true,
+    );
     Get.put<HomeController>(
       HomeController(),
+    );
+    Get.put<TimerController>(
+      TimerController(),
+    );
+    Get.lazyPut<BottomNavigationBarController>(
+      () => BottomNavigationBarController(),
     );
   }
 }
