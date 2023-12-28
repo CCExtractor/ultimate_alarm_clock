bc213b614959b74267f5f129dbfb3fb074c2bdc0
superiorsd10 <sachindapkara6@gmail.com>
2023-12-29 00:19:07 +0530

    adding the timer and bottom-nav-bar controllers in alarm-ring-binding

--- lib/app/modules/alarmRing/bindings/alarm_ring_binding.dart
+++ lib/app/modules/alarmRing/bindings/alarm_ring_binding.dart
@@ -1,6 +1,8 @@
 import 'package:get/get.dart';
+import 'package:ultimate_alarm_clock/app/modules/bottomNavigationBar/controllers/bottom_navigation_bar_controller.dart';
 import 'package:ultimate_alarm_clock/app/modules/home/controllers/home_controller.dart';
 import 'package:ultimate_alarm_clock/app/modules/settings/controllers/settings_controller.dart';
+import 'package:ultimate_alarm_clock/app/modules/timer/controllers/timer_controller.dart';
 
 import '../controllers/alarm_ring_controller.dart';
 
@@ -15,6 +17,12 @@ class AlarmControlBinding extends Bindings {
     );
     Get.lazyPut<HomeController>(
       () => HomeController(),
+    );
+    Get.lazyPut<TimerController>(
+      () => TimerController(),
+    );
+    Get.lazyPut<BottomNavigationBarController>(
+      () => BottomNavigationBarController(),
     );
   }
 }
