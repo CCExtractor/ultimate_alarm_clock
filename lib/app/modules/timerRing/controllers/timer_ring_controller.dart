827f2a2e773ac211f1928339384533bb49311275
superiorsd10 <sachindapkara6@gmail.com>
2023-12-29 00:37:35 +0530

    adding the timerRing module

---
+++ lib/app/modules/timerRing/controllers/timer_ring_controller.dart
@@ -0,0 +1,29 @@
+import 'dart:async';
+
+import 'package:get/get.dart';
+import 'package:ultimate_alarm_clock/app/data/models/alarm_model.dart';
+import 'package:ultimate_alarm_clock/app/utils/utils.dart';
+import 'package:vibration/vibration.dart';
+
+class TimerRingController extends GetxController {
+  final Rx<AlarmModel> currentlyRingingAlarm = Utils.genFakeAlarmModel().obs;
+  Timer? vibrationTimer;
+
+  @override
+  void onInit() {
+    super.onInit();
+    vibrationTimer =
+        Timer.periodic(const Duration(milliseconds: 3500), (Timer timer) {
+      Vibration.vibrate(pattern: [500, 3000]);
+    });
+    Utils.playAlarm(alarmRecord: currentlyRingingAlarm.value);
+  }
+
+  @override
+  void onClose() {
+    Vibration.cancel();
+    vibrationTimer!.cancel();
+    Utils.stopAlarm(ringtoneName: currentlyRingingAlarm.value.ringtoneName);
+    super.onClose();
+  }
+}
