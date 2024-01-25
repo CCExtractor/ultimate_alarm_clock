c4e904f2b88108bcdeeb02fd252288035236c5e8
Rijuth Menon <rijuthm@gmail.com>
2024-01-25 15:51:39 +0530

    added menu for stopwatch

--- lib/app/modules/stopwatch/views/stopwatch_view.dart
+++ lib/app/modules/stopwatch/views/stopwatch_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:ultimate_alarm_clock/app/modules/settings/controllers/theme_controller.dart';
 import 'package:ultimate_alarm_clock/app/modules/stopwatch/controllers/stopwatch_controller.dart';
+import 'package:ultimate_alarm_clock/app/routes/app_pages.dart';
 import '../../../utils/constants.dart';
 import '../../../utils/utils.dart';
 import '../controllers/stopwatch_controller.dart';
@@ -20,21 +21,6 @@ class StopwatchView extends GetView<StopwatchController> {
           toolbarHeight: height / 7.9,
           elevation: 0.0,
           centerTitle: true,
-          actions: [
-            IconButton(
-              onPressed: () {
-                Utils.hapticFeedback();
-                Get.toNamed('/settings');
-              },
-              icon: const Icon(
-                Icons.settings,
-                size: 27,
-              ),
-              color: themeController.isLightMode.value
-                  ? kLightPrimaryTextColor.withOpacity(0.75)
-                  : kprimaryTextColor.withOpacity(0.75),
-            ),
-          ],
         ),
       ),
       body: Center(
@@ -68,6 +54,130 @@ class StopwatchView extends GetView<StopwatchController> {
               ],
             ),
           ],
+        ),
+      ),
+      endDrawer: Obx(
+        () => Drawer(
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(10),
+              bottomLeft: Radius.circular(10),
+            ),
+          ),
+          backgroundColor: themeController.isLightMode.value
+              ? kLightSecondaryBackgroundColor
+              : ksecondaryBackgroundColor,
+          child: Column(
+            children: [
+              DrawerHeader(
+                decoration: const BoxDecoration(color: kLightSecondaryColor),
+                child: Center(
+                  child: Row(
+                    children: [
+                      const Flexible(
+                        flex: 1,
+                        child: CircleAvatar(
+                          radius: 30,
+                          backgroundImage: AssetImage(
+                            'assets/images/ic_launcher-playstore.png',
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Flexible(
+                        flex: 3,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            SizedBox(
+                              width: width * 0.5,
+                              child: Text(
+                                'Ultimate Alarm Clock'.tr,
+                                softWrap: true,
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .displayMedium!
+                                    .copyWith(
+                                      color: themeController.isLightMode.value
+                                          ? kprimaryTextColor
+                                          : ksecondaryTextColor,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                              ),
+                            ),
+                            SizedBox(
+                              width: width * 0.5,
+                              child: Text(
+                                'v0.5.0'.tr,
+                                softWrap: true,
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .titleLarge!
+                                    .copyWith(
+                                      color: themeController.isLightMode.value
+                                          ? kprimaryTextColor
+                                          : ksecondaryTextColor,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              ListTile(
+                onTap: () {
+                  Utils.hapticFeedback();
+                  Get.back();
+                  Get.toNamed('/settings');
+                },
+                contentPadding: const EdgeInsets.only(left: 20, right: 44),
+                title: Text(
+                  'Settings'.tr,
+                  style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                        color: themeController.isLightMode.value
+                            ? kLightPrimaryTextColor.withOpacity(0.8)
+                            : kprimaryTextColor.withOpacity(0.8),
+                      ),
+                ),
+                leading: Icon(
+                  Icons.settings,
+                  size: 26,
+                  color: themeController.isLightMode.value
+                      ? kLightPrimaryTextColor.withOpacity(0.8)
+                      : kprimaryTextColor.withOpacity(0.8),
+                ),
+              ),
+              ListTile(
+                onTap: () {
+                  Utils.hapticFeedback();
+                  Get.back();
+                  Get.toNamed(Routes.ABOUT);
+                },
+                contentPadding: const EdgeInsets.only(left: 20, right: 44),
+                title: Text(
+                  'About'.tr,
+                  style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                        color: themeController.isLightMode.value
+                            ? kLightPrimaryTextColor.withOpacity(0.8)
+                            : kprimaryTextColor.withOpacity(0.8),
+                      ),
+                ),
+                leading: Icon(
+                  Icons.info_outline,
+                  size: 26,
+                  color: themeController.isLightMode.value
+                      ? kLightPrimaryTextColor.withOpacity(0.8)
+                      : kprimaryTextColor.withOpacity(0.8),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
