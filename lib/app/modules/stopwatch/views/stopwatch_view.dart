fab7e43d6c4cb5d27b5ddf44a7ab8d406ea6a62b
Vyshnav <puthen1977@gmail.com>
2024-02-03 16:42:39 +0530

    enhance: Coding Style, encapsulation

--- lib/app/modules/stopwatch/views/stopwatch_view.dart
+++ lib/app/modules/stopwatch/views/stopwatch_view.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 import 'package:ultimate_alarm_clock/app/modules/settings/controllers/theme_controller.dart';
 import 'package:ultimate_alarm_clock/app/modules/stopwatch/controllers/stopwatch_controller.dart';
 import 'package:ultimate_alarm_clock/app/routes/app_pages.dart';
+import 'package:ultimate_alarm_clock/app/utils/end_drawer.dart';
 import '../../../utils/constants.dart';
 import '../../../utils/utils.dart';
 
@@ -139,130 +140,7 @@ class StopwatchView extends GetView<StopwatchController> {
           ),
         ],
       ),
-      endDrawer: Obx(
-        () => Drawer(
-          shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(10),
-              bottomLeft: Radius.circular(10),
-            ),
-          ),
-          backgroundColor: themeController.isLightMode.value
-              ? kLightSecondaryBackgroundColor
-              : ksecondaryBackgroundColor,
-          child: Column(
-            children: [
-              DrawerHeader(
-                decoration: const BoxDecoration(color: kLightSecondaryColor),
-                child: Center(
-                  child: Row(
-                    children: [
-                      const Flexible(
-                        flex: 1,
-                        child: CircleAvatar(
-                          radius: 30,
-                          backgroundImage: AssetImage(
-                            'assets/images/ic_launcher-playstore.png',
-                          ),
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 10,
-                      ),
-                      Flexible(
-                        flex: 3,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            SizedBox(
-                              width: width * 0.5,
-                              child: Text(
-                                'Ultimate Alarm Clock'.tr,
-                                softWrap: true,
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .displayMedium!
-                                    .copyWith(
-                                      color: themeController.isLightMode.value
-                                          ? kprimaryTextColor
-                                          : ksecondaryTextColor,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                              ),
-                            ),
-                            SizedBox(
-                              width: width * 0.5,
-                              child: Text(
-                                'v0.5.0'.tr,
-                                softWrap: true,
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .titleLarge!
-                                    .copyWith(
-                                      color: themeController.isLightMode.value
-                                          ? kprimaryTextColor
-                                          : ksecondaryTextColor,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              ListTile(
-                onTap: () {
-                  Utils.hapticFeedback();
-                  Get.back();
-                  Get.toNamed('/settings');
-                },
-                contentPadding: const EdgeInsets.only(left: 20, right: 44),
-                title: Text(
-                  'Settings'.tr,
-                  style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                        color: themeController.isLightMode.value
-                            ? kLightPrimaryTextColor.withOpacity(0.8)
-                            : kprimaryTextColor.withOpacity(0.8),
-                      ),
-                ),
-                leading: Icon(
-                  Icons.settings,
-                  size: 26,
-                  color: themeController.isLightMode.value
-                      ? kLightPrimaryTextColor.withOpacity(0.8)
-                      : kprimaryTextColor.withOpacity(0.8),
-                ),
-              ),
-              ListTile(
-                onTap: () {
-                  Utils.hapticFeedback();
-                  Get.back();
-                  Get.toNamed(Routes.ABOUT);
-                },
-                contentPadding: const EdgeInsets.only(left: 20, right: 44),
-                title: Text(
-                  'About'.tr,
-                  style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                        color: themeController.isLightMode.value
-                            ? kLightPrimaryTextColor.withOpacity(0.8)
-                            : kprimaryTextColor.withOpacity(0.8),
-                      ),
-                ),
-                leading: Icon(
-                  Icons.info_outline,
-                  size: 26,
-                  color: themeController.isLightMode.value
-                      ? kLightPrimaryTextColor.withOpacity(0.8)
-                      : kprimaryTextColor.withOpacity(0.8),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
+      endDrawer: buildEndDrawer(context)
     );
   }
 }
