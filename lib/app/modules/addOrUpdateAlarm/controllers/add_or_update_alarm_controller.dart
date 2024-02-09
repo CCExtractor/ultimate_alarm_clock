149c9095134820eb656d453420926855845b9a17
tushar khandelwal <144731286+tushar11kh@users.noreply.github.com>
2024-02-09 23:08:06 +0530

    continuation_issue#262

--- lib/app/modules/addOrUpdateAlarm/controllers/add_or_update_alarm_controller.dart
+++ lib/app/modules/addOrUpdateAlarm/controllers/add_or_update_alarm_controller.dart
@@ -79,7 +79,7 @@ class AddOrUpdateAlarmController extends GetxController {
   final RxInt snoozeDuration = 1.obs;
   var customRingtoneName = 'Default'.obs;
   var customRingtoneNames = [].obs;
-  var previousRingtone='';
+  var previousRingtone = '';
   final noteController = TextEditingController();
   final RxString note = ''.obs;
   final deleteAfterGoesOff = false.obs;
@@ -446,7 +446,6 @@ class AddOrUpdateAlarmController extends GetxController {
             showQRDialog();
           }
         },
-
         confirm: TextButton(
           style: ButtonStyle(
             backgroundColor: MaterialStateProperty.all(kprimaryColor),
@@ -458,7 +457,6 @@ class AddOrUpdateAlarmController extends GetxController {
                       ? kLightPrimaryTextColor
                       : ksecondaryTextColor,
                 ),
-
           ),
           onPressed: () async {
             Get.back(); // Close the alert box
@@ -579,7 +577,7 @@ class AddOrUpdateAlarmController extends GetxController {
       );
       hours.value = selectedTime.value.hour;
       minutes.value = selectedTime.value.minute;
-      
+
       if (settingsController.is24HrsEnabled.value == false) {
         if (selectedTime.value.hour == 0) {
           hours.value = 12;
@@ -871,6 +869,9 @@ class AddOrUpdateAlarmController extends GetxController {
             ringtonePath: savedFilePath,
             currentCounterOfUsage: 1,
           );
+          AudioUtils.updateRingtoneCounterOfUsage(
+              customRingtoneName: previousRingtone,
+              counterUpdate: CounterUpdate.decrement);
           await IsarDb.addCustomRingtone(customRingtone);
         }
       }
