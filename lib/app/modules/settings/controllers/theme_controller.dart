f6d4f29d186be147644d82bc66c9683887a0a9e8
Vyshnav <puthen1977@gmail.com>
2024-02-15 23:57:38 +0530

    feat: Listener added

--- lib/app/modules/settings/controllers/theme_controller.dart
+++ lib/app/modules/settings/controllers/theme_controller.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:ultimate_alarm_clock/app/data/providers/secure_storage_provider.dart';
@@ -7,16 +8,55 @@ class ThemeController extends GetxController {
   var isLightMode = true.obs;
   final _secureStorageProvider = SecureStorageProvider();
 
+  late Timer _brightnessCheckTimer;
+
   @override
   void onInit() {
     _loadThemeValue();
+    _startBrightnessCheckTimer();
     super.onInit();
   }
 
+  @override
+  void onClose() {
+    _brightnessCheckTimer.cancel();
+    super.onClose();
+  }
+
   void _loadThemeValue() async {
-    isLightMode.value =
-        await _secureStorageProvider.readThemeValue() == AppTheme.light;
+    final storedTheme = await _secureStorageProvider.readThemeValue();
+  final systemBrightness = Get.mediaQuery.platformBrightness;
+
+  if (storedTheme == AppTheme.system) {
+    if (systemBrightness == Brightness.light) {
+      isLightMode.value = true;
+      Get.changeThemeMode(ThemeMode.light);
+    } else {
+      isLightMode.value = false;
+      Get.changeThemeMode(ThemeMode.dark);
+    }
+  } else {
+    isLightMode.value = storedTheme == AppTheme.light;
     Get.changeThemeMode(isLightMode.value ? ThemeMode.light : ThemeMode.dark);
+  }
+  }
+
+  void _startBrightnessCheckTimer() {
+    _brightnessCheckTimer = Timer.periodic(Duration(seconds: 2), (timer) {
+      _updateThemeFromSystemBrightness();
+    });
+  }
+
+  void _updateThemeFromSystemBrightness() {
+    final systemBrightness = Get.mediaQuery.platformBrightness;
+
+    if (systemBrightness == Brightness.light) {
+      isLightMode.value = true;
+      Get.changeThemeMode(ThemeMode.light);
+    } else {
+      isLightMode.value = false;
+      Get.changeThemeMode(ThemeMode.dark);
+    }
   }
 
   void _saveThemeValuePreference() async {
@@ -28,5 +68,20 @@ class ThemeController extends GetxController {
   void toggleThemeValue(bool enabled) {
     isLightMode.value = enabled;
     _saveThemeValuePreference();
+  }
+
+  void toggleSystemTheme() {
+    final systemBrightness = Get.mediaQuery.platformBrightness;
+  print("System Brightness: $systemBrightness");
+  _saveThemeValuePreference();
+
+  if (systemBrightness == Brightness.light) {
+    isLightMode.value = true;
+    Get.changeThemeMode(ThemeMode.light);
+  } else {isLightMode.value = false;
+    Get.changeThemeMode(ThemeMode.dark);
+  }
+    _saveThemeValuePreference();
+    _updateThemeFromSystemBrightness();
   }
 }
--- lib/app/modules/settings/views/theme_value_tile.dart
+++ lib/app/modules/settings/views/theme_value_tile.dart
@@ -7,16 +7,15 @@ import 'package:ultimate_alarm_clock/app/utils/utils.dart';
 
 class ThemeValueTile extends StatefulWidget {
   const ThemeValueTile({
-    super.key,
+    Key? key,
     required this.controller,
     required this.height,
     required this.width,
     required this.themeController,
-  });
+  }) : super(key: key);
 
   final SettingsController controller;
   final ThemeController themeController;
-
   final double height;
   final double width;
 
@@ -27,41 +26,87 @@ class ThemeValueTile extends StatefulWidget {
 class _ThemeValueTileState extends State<ThemeValueTile> {
   @override
   Widget build(BuildContext context) {
-    return Obx(
-      () => Container(
-        width: widget.width * 0.91,
-        height: widget.height * 0.1,
-        decoration: Utils.getCustomTileBoxDecoration(
-          isLightMode: widget.themeController.isLightMode.value,
-        ),
-        child: Padding(
-          padding: EdgeInsets.only(left: 30, right: 20),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Expanded(
-                child: Text(
-                  'Enable Light Mode'.tr,
-                  style: Theme.of(context).textTheme.bodyLarge,
+    return Container(
+      width: widget.width * 0.91,
+      height: widget.height * 0.1,
+      decoration: Utils.getCustomTileBoxDecoration(
+        isLightMode: widget.themeController.isLightMode.value,
+      ),
+      child: Padding(
+        padding: EdgeInsets.only(left: 30),
+        child: Row(
+          children: [
+            DropdownMenu(
+              menuStyle: MenuStyle(
+                backgroundColor: MaterialStatePropertyAll(
+                  widget.themeController.isLightMode.value
+                      ? kLightSecondaryBackgroundColor
+                      : ksecondaryBackgroundColor,
                 ),
               ),
-              Obx(
-                () => Switch.adaptive(
-                  value: widget.themeController.isLightMode.value,
-                  activeColor: ksecondaryColor,
-                  onChanged: (bool value) async {
-                    widget.themeController.toggleThemeValue(value);
-                    Get.changeThemeMode(
+              inputDecorationTheme:
+                  InputDecorationTheme(enabledBorder: InputBorder.none),
+              trailingIcon: Icon(
+                Icons.arrow_drop_down_outlined,
+                size: 40.0,
+                color: widget.themeController.isLightMode.value
+                    ? kLightPrimaryTextColor.withOpacity(0.8)
+                    : kprimaryTextColor.withOpacity(0.8),
+              ),
+              width: widget.width * 0.78,
+              initialSelection: widget.themeController.isLightMode.value
+                  ? 'Light Mode'
+                  : 'Dark Mode',
+              label: Text('Select Theme'),
+              dropdownMenuEntries: [
+                DropdownMenuEntry(
+                  value: 'System Mode',
+                  label: 'System Mode',
+                  style: ButtonStyle(
+                    foregroundColor: MaterialStatePropertyAll(
                       widget.themeController.isLightMode.value
-                          ? ThemeMode.light
-                          : ThemeMode.dark,
-                    );
-                    Utils.hapticFeedback();
-                  },
+                          ? kLightPrimaryTextColor
+                          : kprimaryTextColor,
+                    ),
+                  ),
                 ),
-              ),
-            ],
-          ),
+                DropdownMenuEntry(
+                  value: 'Light Mode',
+                  label: 'Light Mode',
+                  style: ButtonStyle(
+                    foregroundColor: MaterialStatePropertyAll(
+                      widget.themeController.isLightMode.value
+                          ? kLightPrimaryTextColor
+                          : kprimaryTextColor,
+                    ),
+                  ),
+                ),
+                DropdownMenuEntry(
+                  value: 'Dark Mode',
+                  label: 'Dark Mode',
+                  style: ButtonStyle(
+                    foregroundColor: MaterialStatePropertyAll(
+                      widget.themeController.isLightMode.value
+                          ? kLightPrimaryTextColor
+                          : kprimaryTextColor,
+                    ),
+                  ),
+                ),
+              ],
+              onSelected: (newValue) {
+                if (newValue == 'System Mode') {
+                  widget.themeController.toggleSystemTheme();
+                } else if (newValue == 'Light Mode') {
+                  widget.themeController.toggleThemeValue(true);
+                  Get.changeThemeMode(ThemeMode.light);
+                } else {
+                  widget.themeController.toggleThemeValue(false);
+                  Get.changeThemeMode(ThemeMode.dark);
+                }
+                Utils.hapticFeedback();
+              },
+            ),
+          ],
         ),
       ),
     );
