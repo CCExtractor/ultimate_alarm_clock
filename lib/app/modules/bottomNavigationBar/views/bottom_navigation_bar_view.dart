bcee27002f9d399133daf07174d619f468f6c419
Nidhin Ninan <nidhinvninan29@gmail.com>
2024-02-26 00:59:51 +0530

    Modified bottom_navigation_bar_view.dart

--- lib/app/modules/bottomNavigationBar/views/bottom_navigation_bar_view.dart
+++ lib/app/modules/bottomNavigationBar/views/bottom_navigation_bar_view.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:ultimate_alarm_clock/app/modules/bottomNavigationBar/controllers/bottom_navigation_bar_controller.dart';
+import 'package:ultimate_alarm_clock/app/modules/settings/controllers/theme_controller.dart';
+import 'package:ultimate_alarm_clock/app/utils/constants.dart';
 import 'package:ultimate_alarm_clock/app/utils/utils.dart';
 
 class BottomNavigationBarView extends GetView<BottomNavigationBarController> {
   final PageController pageController = PageController();
+  final ThemeController themeController = Get.find<ThemeController>();
 
+  BottomNavigationBarView({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,28 +25,38 @@ class BottomNavigationBarView extends GetView<BottomNavigationBarController> {
           useLegacyColorScheme: false,
           items: [
             BottomNavigationBarItem(
-              icon: Icon(Icons.alarm),
+              icon: const Icon(Icons.alarm),
               label: 'Alarm'.tr,
             ),
             BottomNavigationBarItem(
-              icon: Icon(Icons.timer_outlined),
+              icon: const Icon(Icons.timer_outlined),
               label: 'StopWatch'.tr,
             ),
             BottomNavigationBarItem(
-              icon: Icon(Icons.timelapse_outlined),
+              icon: const Icon(Icons.timelapse_outlined),
               label: 'Timer'.tr,
             ),
           ],
           onTap: (index) {
             Utils.hapticFeedback();
             controller.changeTab(index);
-            pageController.animateToPage(index,
-                duration: Duration(milliseconds: 300), curve: Curves.easeInOut);
+            pageController.animateToPage(
+              index,
+              duration: const Duration(milliseconds: 300),
+              curve: Curves.easeInOut,
+            );
           },
           currentIndex: controller.activeTabIndex.value,
+          selectedLabelStyle: TextStyle(
+            color: kprimaryColor,
+            fontSize: 14,
+            decoration: TextDecoration.underline,
+            decorationColor:
+                themeController.isLightMode.value ? Colors.black : Colors.white,
+            decorationThickness: 0.8,
+          ),
         ),
       ),
     );
   }
 }
-
