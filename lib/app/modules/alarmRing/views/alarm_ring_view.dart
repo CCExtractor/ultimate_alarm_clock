03aaa02bf6e629a9d26d10afa426f1a13fd57ef9
Vansh Gupta <149355139+codesage01@users.noreply.github.com>
2024-03-03 10:23:41 +0530

    Update alarm_ring_view.dart

--- lib/app/modules/alarmRing/views/alarm_ring_view.dart
+++ lib/app/modules/alarmRing/views/alarm_ring_view.dart
@@ -1,290 +1,221 @@
-import 'dart:async';
-import 'dart:math';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-
-import 'package:flutter_fgbg/flutter_fgbg.dart';
-import 'package:flutter_volume_controller/flutter_volume_controller.dart';
 
 import 'package:get/get.dart';
-import 'package:ultimate_alarm_clock/app/data/models/alarm_model.dart';
-
-import 'package:ultimate_alarm_clock/app/data/models/user_model.dart';
-
-import 'package:ultimate_alarm_clock/app/data/providers/firestore_provider.dart';
-import 'package:ultimate_alarm_clock/app/data/providers/isar_provider.dart';
-import 'package:ultimate_alarm_clock/app/data/providers/secure_storage_provider.dart';
-import 'package:ultimate_alarm_clock/app/utils/audio_utils.dart';
-
+import 'package:ultimate_alarm_clock/app/modules/settings/controllers/theme_controller.dart';
+import 'package:ultimate_alarm_clock/app/utils/constants.dart';
 import 'package:ultimate_alarm_clock/app/utils/utils.dart';
-import 'package:vibration/vibration.dart';
 
-class AlarmControlController extends GetxController {
-  MethodChannel alarmChannel = MethodChannel('ulticlock');
-  RxString note = ''.obs;
-  Timer? vibrationTimer;
-  late StreamSubscription<FGBGType> _subscription;
-  TimeOfDay currentTime = TimeOfDay.now();
-  late RxBool isSnoozing = false.obs;
-  RxInt minutes = 1.obs;
-  RxInt seconds = 0.obs;
-  RxBool showButton = false.obs;
-  final Rx<AlarmModel> currentlyRingingAlarm = Utils.genFakeAlarmModel().obs;
-  final formattedDate = Utils.getFormattedDate(DateTime.now()).obs;
-  final timeNow =
-      Utils.convertTo12HourFormat(Utils.timeOfDayToString(TimeOfDay.now())).obs;
-  Timer? _currentTimeTimer;
-  bool isAlarmActive = true;
-  late double initialVolume;
+import '../controllers/alarm_ring_controller.dart';
 
-  getCurrentlyRingingAlarm() async {
-    UserModel? _userModel = await SecureStorageProvider().retrieveUserModel();
-    AlarmModel _alarmRecord = Utils.genFakeAlarmModel();
-    AlarmModel isarLatestAlarm =
-        await IsarDb.getLatestAlarm(_alarmRecord, false);
-    AlarmModel firestoreLatestAlarm =
-        await FirestoreDb.getLatestAlarm(_userModel, _alarmRecord, false);
-    AlarmModel latestAlarm =
-        Utils.getFirstScheduledAlarm(isarLatestAlarm, firestoreLatestAlarm);
-    debugPrint('CURRENT RINGING : ${latestAlarm.alarmTime}');
+class AlarmControlView extends GetView<AlarmControlController> {
+  AlarmControlView({Key? key}) : super(key: key);
 
-    return latestAlarm;
-  }
+  ThemeController themeController = Get.find<ThemeController>();
 
-  getAlarm() async {
-    UserModel? _userModel = await SecureStorageProvider().retrieveUserModel();
-    AlarmModel _alarmRecord = Utils.genFakeAlarmModel();
-    AlarmModel isarLatestAlarm =
-        await IsarDb.getLatestAlarm(_alarmRecord, true);
-    AlarmModel firestoreLatestAlarm =
-        await FirestoreDb.getLatestAlarm(_userModel, _alarmRecord, true);
-    AlarmModel latestAlarm =
-        Utils.getFirstScheduledAlarm(isarLatestAlarm, firestoreLatestAlarm);
-    debugPrint('LATEST : ${latestAlarm.alarmTime}');
+  @override
+  Widget build(BuildContext context) {
+    var width = Get.width;
+    var height = Get.height;
+    return PopScope(
+      canPop: false,
+      onPopInvoked: (bool didPop) {
+        if (didPop) {
+          return;
+        }
 
-    return latestAlarm;
-  }
-
-  void startSnooze() async {
-    Vibration.cancel();
-    vibrationTimer!.cancel();
-    isSnoozing.value = true;
-    String ringtoneName = currentlyRingingAlarm.value.ringtoneName;
-    AudioUtils.stopAlarm(ringtoneName: ringtoneName);
-
-    if (_currentTimeTimer!.isActive) {
-      _currentTimeTimer?.cancel();
-    }
-
-    _currentTimeTimer = Timer.periodic(const Duration(seconds: 1), (timer) {
-      if (minutes.value == 0 && seconds.value == 0) {
-        timer.cancel();
-        vibrationTimer =
-            Timer.periodic(const Duration(milliseconds: 3500), (Timer timer) {
-          Vibration.vibrate(pattern: [500, 3000]);
-        });
-
-        AudioUtils.playAlarm(alarmRecord: currentlyRingingAlarm.value);
-
-        startTimer();
-      } else if (seconds.value == 0) {
-        minutes.value--;
-        seconds.value = 59;
-      } else {
-        seconds.value--;
-      }
-    });
-  }
-
-  void startTimer() {
-    minutes.value = currentlyRingingAlarm.value.snoozeDuration;
-    isSnoozing.value = false;
-    _currentTimeTimer = Timer.periodic(
-        Duration(
-          milliseconds: Utils.getMillisecondsToAlarm(
-            DateTime.now(),
-            DateTime.now().add(const Duration(minutes: 1)),
+        Get.snackbar(
+          'Note'.tr,
+          "You can't go back while the alarm is ringing".tr,
+          backgroundColor: Colors.red,
+          colorText: Colors.white,
+        );
+      },
+      child: SafeArea(
+        child: Scaffold(
+          floatingActionButtonLocation:
+              FloatingActionButtonLocation.centerDocked,
+          floatingActionButton: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(18.0),
+                child: Obx(
+                  () => SizedBox(
+                    height: height * 0.06,
+                    width: width * 0.8,
+                    child: controller.showButton.value
+                        ? TextButton(
+                            style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all(
+                                kprimaryColor,
+                              ),
+                            ),
+                            child: Text(
+                              Utils.isChallengeEnabled(
+                                controller.currentlyRingingAlarm.value,
+                              )
+                                  ? 'Start Challenge'.tr
+                                  : 'Dismiss'.tr,
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .displaySmall!
+                                  .copyWith(
+                                    color: themeController.isLightMode.value
+                                        ? kLightPrimaryTextColor
+                                        : ksecondaryTextColor,
+                                  ),
+                            ),
+                            onPressed: () {
+                              Utils.hapticFeedback();
+                              if (Utils.isChallengeEnabled(
+                                controller.currentlyRingingAlarm.value,
+                              )) {
+                                Get.toNamed(
+                                  '/alarm-challenge',
+                                  arguments:
+                                      controller.currentlyRingingAlarm.value,
+                                );
+                              } else {
+                                Get.offNamed(
+                                  '/bottom-navigation-bar',
+                                  arguments: controller.currentlyRingingAlarm
+                                      .value.showMotivationalQuote,
+                                );
+                              }
+                            },
+                          )
+                        : const SizedBox(),
+                  ),
+                ),
+              ),
+              (Get.arguments != null)
+                  ? Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Obx(
+                        () => SizedBox(
+                          height: height * 0.06,
+                          width: width,
+                          child: TextButton(
+                            style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all(
+                                themeController.isLightMode.value
+                                    ? kLightPrimaryTextColor.withOpacity(0.7)
+                                    : kprimaryTextColor.withOpacity(0.7),
+                              ),
+                            ),
+                            child: Text(
+                              'Exit Preview'.tr,
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .displaySmall!
+                                  .copyWith(
+                                    color: themeController.isLightMode.value
+                                        ? kLightPrimaryTextColor
+                                        : ksecondaryTextColor,
+                                  ),
+                            ),
+                            onPressed: () {
+                              Utils.hapticFeedback();
+                              Get.offNamed('/bottom-navigation-bar');
+                            },
+                          ),
+                        ),
+                      ),
+                    )
+                  : const SizedBox(),
+            ],
           ),
-        ), (timer) {
-      formattedDate.value = Utils.getFormattedDate(DateTime.now());
-      timeNow.value =
-          Utils.convertTo12HourFormat(Utils.timeOfDayToString(TimeOfDay.now()));
-    });
-  }
-
-  Future<void> _fadeInAlarmVolume() async {
-    await FlutterVolumeController.setVolume(
-      currentlyRingingAlarm.value.volMin / 10.0,
-      stream: AudioStream.alarm,
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Obx(
+                  () => Column(
+                    children: [
+                      Text(
+                        controller.formattedDate.value,
+                        style: Theme.of(context).textTheme.bodyLarge,
+                      ),
+                      const SizedBox(
+                        height: 10,
+                        width: 0,
+                      ),
+                      Text(
+                        (controller.isSnoozing.value)
+                            ? "${controller.minutes.toString().padLeft(2, '0')}"
+                                // ignore: lines_longer_than_80_chars
+                                ":${controller.seconds.toString().padLeft(2, '0')}"
+                            : '${controller.timeNow[0]} '
+                                '${controller.timeNow[1]}',
+                        style: Theme.of(context)
+                            .textTheme
+                            .displayLarge!
+                            .copyWith(fontSize: 50),
+                      ),
+                    ],
+                  ),
+                ),
+                Obx(
+                  () => Visibility(
+                    visible:
+                        controller.currentlyRingingAlarm.value.note.isNotEmpty,
+                    child: Text(
+                      controller.currentlyRingingAlarm.value.note,
+                      style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                            color: themeController.isLightMode.value
+                                ? kLightPrimaryTextColor
+                                : kprimaryTextColor,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w100,
+                            fontStyle: FontStyle.italic,
+                          ),
+                    ),
+                  ),
+                ),
+                Obx(
+                  () => Visibility(
+                    visible: !controller.isSnoozing.value,
+                    child: Obx(
+                      () => Padding(
+                        padding: Get.arguments != null
+                            ? const EdgeInsets.symmetric(vertical: 90.0)
+                            : EdgeInsets.zero,
+                        child: SizedBox(
+                          height: height * 0.07,
+                          width: width * 0.5,
+                          child: TextButton(
+                            style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all(
+                                themeController.isLightMode.value
+                                    ? kLightSecondaryBackgroundColor
+                                    : ksecondaryBackgroundColor,
+                              ),
+                            ),
+                            child: Text(
+                              'Snooze'.tr,
+                              style:
+                                  Theme.of(context).textTheme.bodyMedium!.copyWith(
+                                        color: themeController.isLightMode.value
+                                            ? kLightPrimaryTextColor
+                                            : kprimaryTextColor,
+                                        fontWeight: FontWeight.w600,
+                                      ),
+                            ),
+                            onPressed: () {
+                              Utils.hapticFeedback();
+                              controller.startSnooze();
+                            },
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
-    await Future.delayed(const Duration(milliseconds: 2000));
-
-    double vol = currentlyRingingAlarm.value.volMin / 10.0;
-    double diff = (currentlyRingingAlarm.value.volMax -
-            currentlyRingingAlarm.value.volMin) /
-        10.0;
-    int len = currentlyRingingAlarm.value.gradient * 1000;
-    double steps = (diff / 0.01).abs();
-    int stepLen = max(4, (steps > 0) ? len ~/ steps : len);
-    int lastTick = DateTime.now().millisecondsSinceEpoch;
-
-    Timer.periodic(Duration(milliseconds: stepLen), (Timer t) {
-      if (!isAlarmActive) {
-        t.cancel();
-        return;
-      }
-
-      var now = DateTime.now().millisecondsSinceEpoch;
-      var tick = (now - lastTick) / len;
-      lastTick = now;
-      vol += diff * tick;
-
-      vol = max(currentlyRingingAlarm.value.volMin / 10.0, vol);
-      vol = min(currentlyRingingAlarm.value.volMax / 10.0, vol);
-      vol = (vol * 100).round() / 100;
-
-      FlutterVolumeController.setVolume(
-        vol,
-        stream: AudioStream.alarm,
-      );
-
-      if (vol >= currentlyRingingAlarm.value.volMax / 10.0) {
-        t.cancel();
-      }
-    });
-  }
-
-  @override
-  void onInit() async {
-    super.onInit();
-    initialVolume = await FlutterVolumeController.getVolume(
-      stream: AudioStream.alarm,
-    ) as double;
-
-    FlutterVolumeController.updateShowSystemUI(false);
-
-    _fadeInAlarmVolume();
-
-    if (currentlyRingingAlarm.value.deleteAfterGoesOff == true) {
-      if (currentlyRingingAlarm.value.isSharedAlarmEnabled) {
-        FirestoreDb.deleteOneTimeAlarm(
-          currentlyRingingAlarm.value.ownerId,
-          currentlyRingingAlarm.value.firestoreId,
-        );
-      } else {
-        IsarDb.deleteAlarm(currentlyRingingAlarm.value.isarId);
-      }
-    }
-    vibrationTimer =
-        Timer.periodic(const Duration(milliseconds: 3500), (Timer timer) {
-      Vibration.vibrate(pattern: [500, 3000]);
-    });
-
-    // Preventing app from being minimized!
-    _subscription = FGBGEvents.stream.listen((event) {
-      if (event == FGBGType.background) {
-        alarmChannel.invokeMethod('bringAppToForeground');
-      }
-    });
-
-    startTimer();
-    if (Get.arguments == null) {
-      currentlyRingingAlarm.value = await getCurrentlyRingingAlarm();
-      showButton.value = true;
-      // If the alarm is set to NEVER repeat, then it will
-      // be chosen as the  alarm to ring by default as
-      // it would ring the  day
-      if (currentlyRingingAlarm.value.days
-          .every((element) => element == false)) {
-        currentlyRingingAlarm.value.isEnabled = false;
-
-        if (currentlyRingingAlarm.value.isSharedAlarmEnabled == false) {
-          IsarDb.updateAlarm(currentlyRingingAlarm.value);
-        } else {
-          FirestoreDb.updateAlarm(
-            currentlyRingingAlarm.value.ownerId,
-            currentlyRingingAlarm.value,
-          );
-        }
-      } else if (currentlyRingingAlarm.value.isOneTime == true) {
-        // If the alarm has to repeat on one day, but ring just once, we will
-        // keep seting its days to false until it will never ring
-        int currentDay = DateTime.now().weekday - 1;
-        currentlyRingingAlarm.value.days[currentDay] = false;
-
-        if (currentlyRingingAlarm.value.days
-            .every((element) => element == false)) {
-          currentlyRingingAlarm.value.isEnabled = false;
-        }
-
-        if (currentlyRingingAlarm.value.isSharedAlarmEnabled == false) {
-          IsarDb.updateAlarm(currentlyRingingAlarm.value);
-        } else {
-          FirestoreDb.updateAlarm(
-            currentlyRingingAlarm.value.ownerId,
-            currentlyRingingAlarm.value,
-          );
-        }
-      }
-    } else {
-      currentlyRingingAlarm.value = Get.arguments;
-      showButton.value = true;
-    }
-
-    AudioUtils.playAlarm(alarmRecord: currentlyRingingAlarm.value);
-
-    // Setting snooze duration
-    minutes.value = currentlyRingingAlarm.value.snoozeDuration;
-
-    // Scheduling  alarm if it's not in preview mode
-    if (Get.arguments == null) {
-      // Finding the  alarm to ring
-      AlarmModel latestAlarm = await getAlarm();
-      TimeOfDay latestAlarmTimeOfDay =
-          Utils.stringToTimeOfDay(latestAlarm.alarmTime);
-
-      // }
-      // This condition will never satisfy because this will only
-      // occur if fake model is returned as latest alarm
-      if (latestAlarm.isEnabled == false) {
-        debugPrint(
-          'STOPPED IF CONDITION with latest = '
-          '${latestAlarmTimeOfDay.toString()} and '
-          'current = ${currentTime.toString()}',
-        );
-
-        await alarmChannel.invokeMethod('cancelAllScheduledAlarms');
-      } else {
-        int intervaltoAlarm = Utils.getMillisecondsToAlarm(
-          DateTime.now(),
-          Utils.timeOfDayToDateTime(latestAlarmTimeOfDay),
-        );
-
-        try {
-          await alarmChannel
-              .invokeMethod('scheduleAlarm', {'milliSeconds': intervaltoAlarm});
-          print("Scheduled...");
-        } on PlatformException catch (e) {
-          print("Failed to schedule alarm: ${e.message}");
-        }
-      }
-    }
-  }
-
-  @override
-  void onClose() async {
-    super.onClose();
-    Vibration.cancel();
-    vibrationTimer!.cancel();
-    isAlarmActive = false;
-    String ringtoneName = currentlyRingingAlarm.value.ringtoneName;
-    AudioUtils.stopAlarm(ringtoneName: ringtoneName);
-    await FlutterVolumeController.setVolume(
-      initialVolume,
-      stream: AudioStream.alarm,
-    );
-    _subscription.cancel();
-    _currentTimeTimer?.cancel();
   }
 }
