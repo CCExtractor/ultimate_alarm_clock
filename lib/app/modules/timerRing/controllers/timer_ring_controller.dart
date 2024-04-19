6b40a57a9c0ddce6c4fc2ed269b6a22a83f81a32
vkprogrammer-001 <vipinkumar212003@gmail.com>
2024-04-19 23:17:07 +0530

    restart feature added

--- lib/app/modules/timerRing/controllers/timer_ring_controller.dart
+++ lib/app/modules/timerRing/controllers/timer_ring_controller.dart
@@ -3,7 +3,6 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 import 'package:flutter_fgbg/flutter_fgbg.dart';
 import 'package:get/get.dart';
-import 'package:ultimate_alarm_clock/app/data/models/alarm_model.dart';
 import 'package:ultimate_alarm_clock/app/data/models/timer_model.dart';
 import 'package:ultimate_alarm_clock/app/utils/audio_utils.dart';
 import 'package:ultimate_alarm_clock/app/utils/utils.dart';
@@ -14,6 +13,63 @@ class TimerRingController extends GetxController {
   final Rx<TimerModel> currentlyRingingAlarm = Utils.genFakeTimerModel().obs;
   Timer? vibrationTimer;
   late StreamSubscription<FGBGType> _subscription;
+  late RxBool isStart = false.obs;
+  RxInt minutes = 1.obs;
+  RxInt seconds = 0.obs;
+  RxBool showButton = false.obs;
+  final formattedDate = Utils.getFormattedDate(DateTime.now()).obs;
+  Timer? _currentTimeTimer;
+  bool isAlarmActive = true;
+
+  void addMinutes(int incrementMinutes) {
+    minutes.value += incrementMinutes;
+  }
+  
+  void reStartTimer() async {
+    Vibration.cancel();
+    vibrationTimer!.cancel();
+    isStart.value = true;
+    String ringtoneName = currentlyRingingAlarm.value.ringtoneName;
+    AudioUtils.stopAlarm(ringtoneName: ringtoneName);
+
+    if (_currentTimeTimer!.isActive) {
+      _currentTimeTimer?.cancel();
+    }
+
+    _currentTimeTimer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      if (minutes.value == 0 && seconds.value == 0) {
+        timer.cancel();
+        vibrationTimer =
+            Timer.periodic(const Duration(milliseconds: 3500), (Timer timer) {
+          Vibration.vibrate(pattern: [500, 3000]);
+        });
+
+        AudioUtils.playTimer(alarmRecord: currentlyRingingAlarm.value);
+
+        startTimer();
+      } else if (seconds.value == 0) {
+        minutes.value--;
+        seconds.value = 59;
+      } else {
+        seconds.value--;
+      }
+    });
+  }
+
+  void startTimer() {
+    minutes.value = 1;
+    isStart.value = false;
+    _currentTimeTimer = Timer.periodic(
+        Duration(
+          milliseconds: Utils.getMillisecondsToAlarm(
+            DateTime.now(),
+            DateTime.now().add(const Duration(minutes: 1)),
+          ),
+        ), (timer) {
+      formattedDate.value = Utils.getFormattedDate(DateTime.now());
+    });
+  }
+
   @override
   void onInit() async {
     super.onInit();
@@ -28,6 +84,7 @@ class TimerRingController extends GetxController {
         Timer.periodic(const Duration(milliseconds: 3500), (Timer timer) {
       Vibration.vibrate(pattern: [500, 3000]);
     });
+    startTimer();
     AudioUtils.playTimer(alarmRecord: currentlyRingingAlarm.value);
 
     await timerChannel.invokeMethod('cancelTimer');
@@ -35,12 +92,14 @@ class TimerRingController extends GetxController {
 
   @override
   void onClose() {
+    super.onClose();
     Vibration.cancel();
     vibrationTimer!.cancel();
+    isAlarmActive = false;
     AudioUtils.stopTimer(
       ringtoneName: currentlyRingingAlarm.value.ringtoneName,
     );
     _subscription.cancel();
-    super.onClose();
+    _currentTimeTimer?.cancel();
   }
 }
