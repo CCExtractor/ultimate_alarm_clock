7bd8b6118ca63cd46dbd75709e3844d6d54bbece
mahendra kasula <kasulamahi624@gmail.com>
2025-02-22 23:44:54 +0530

    Update snooze_duration_tile.dart
    the final code of snooze

--- lib/app/modules/addOrUpdateAlarm/views/snooze_duration_tile.dart
+++ lib/app/modules/addOrUpdateAlarm/views/snooze_duration_tile.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:numberpicker/numberpicker.dart';
 import 'package:ultimate_alarm_clock/app/modules/addOrUpdateAlarm/controllers/add_or_update_alarm_controller.dart';
 import 'package:ultimate_alarm_clock/app/modules/settings/controllers/theme_controller.dart';
-import 'package:ultimate_alarm_clock/app/utils/constants.dart';
 import 'package:ultimate_alarm_clock/app/utils/utils.dart';
 
 class SnoozeDurationTile extends StatelessWidget {
@@ -18,80 +16,11 @@ class SnoozeDurationTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    int duration;
     return Obx(
       () => InkWell(
         onTap: () {
           Utils.hapticFeedback();
-          // storing the values
-          duration = controller.snoozeDuration.value;
-          // Ensure the snooze duration is within valid range
-          if (duration < 1) {
-            duration = 1;
-            controller.snoozeDuration.value = 1;
-          }
-          Get.defaultDialog(
-            onWillPop: () async {
-              Get.back();
-              // presetting the value to its initial state
-              controller.snoozeDuration.value = duration;
-              return true;
-            },
-            titlePadding: const EdgeInsets.only(top: 20),
-            backgroundColor: themeController.secondaryBackgroundColor.value,
-            title: 'Select duration'.tr,
-            titleStyle: Theme.of(context).textTheme.displaySmall,
-            content: SingleChildScrollView(
-              child: ConstrainedBox(
-                constraints: BoxConstraints(maxHeight: MediaQuery.of(context).size.height * 0.5),
-                child: Obx(
-                  () => Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 10.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            NumberPicker(
-                              value: controller.snoozeDuration.value,
-                              minValue: 1,
-                              maxValue: 1440,
-                              onChanged: (value) {
-                                Utils.hapticFeedback();
-                                controller.snoozeDuration.value = value;
-                              },
-                            ),
-                            Text(
-                              controller.snoozeDuration.value > 1
-                                  ? 'minutes'.tr
-                                  : 'minute'.tr,
-                            ),
-                          ],
-                        ),
-                      ),
-                      ElevatedButton(
-                        onPressed: () {
-                          Utils.hapticFeedback();
-                          Get.back();
-                        },
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: kprimaryColor,
-                        ),
-                        child: Text(
-                          'Done'.tr,
-                          style: Theme.of(context).textTheme.displaySmall!.copyWith(
-                                color: themeController.secondaryTextColor.value,
-                              ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          );
+          controller.openSnoozeDurationDialog(context, themeController);
         },
         child: ListTile(
           title: FittedBox(
