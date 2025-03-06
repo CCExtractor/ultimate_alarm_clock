c156fed5129623d686a43dc31b3c37c9b312d236
Kush Choudhary <kushchoudhary@Kushs-MacBook-Pro.local>
2025-03-06 22:20:27 +0530

    added alarm scheduling logic

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -2,6 +2,7 @@ import Flutter
 import UIKit
 import AVFoundation
 import UserNotifications
+import FMDB
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -25,6 +26,8 @@ import UserNotifications
       let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
       self.requestNotificationAuth()
       
+      NSLog("Log:HERE I AM")
+      
       let methodChannel1 = FlutterMethodChannel(
         name: AppDelegate.CHANNEL1,
         binaryMessenger: controller.binaryMessenger
@@ -36,28 +39,62 @@ import UserNotifications
       
       methodChannel1.setMethodCallHandler { (call, result) in
           switch call.method {
-              case "scheduleAlarm":
-                  print("FLUTTER CALLED SCHEDULE")
-                  result(nil)
-              case "cancelAllScheduledAlarms":
-                  print("FLUTTER CALLED CANCEL ALARMS")
-                  result(nil)
-              case "bringAppToForeground":
-                  print("BRING APP FOREGROUND")
-                  //self.bringAppToForeground()
-                  result(nil)
-              case "minimizeApp":
-                  print("APP MINIMIZED")
-                  //self.minimizeApp()
-                  result(nil)
-              case "playDefaultAlarm":
-                  self.playDefaultAlarm()
-                  result(nil)
-              case "stopDefaultAlarm":
-                  self.stopDefaultAlarm()
-                  result(nil)
-              default:
-                  result(FlutterMethodNotImplemented)
+          case "scheduleAlarm":
+              NSLog("Log:FLUTTER CALLED SCHEDULE")
+              var dbHelper: DatabaseHelper = DatabaseHelper()
+              var db: FMDatabase? = dbHelper.database
+              // get profile from the flutter side
+              let profile = "Default"
+              var ringTime: [String: Any]? = getLatestAlarm(db: db, profile: profile)
+              
+              guard let interval = ringTime?["interval"] else {
+                  NSLog("Log:Something Wrong")
+                  NSLog("FLUTTER CALLED CANCEL ALARMS")
+                  self.cancelAllScheduledAlarms()
+                  return
+              }
+              guard let ringTime = ringTime else {
+                  print("ringTime is nil")
+                  return
+              }
+              NSLog("Log:TIME TO ALARM(seconds): \(interval)")
+              self.scheduleAlarm(
+                alarmID: ringTime["alarmID"] as! String,
+                interval: ringTime["interval"] as! Double,
+                isActivity: ringTime["isActivity"] as! Bool,
+                isLocation: ringTime["isLocation"] as! Bool,
+                location: ringTime["location"] as! String,
+                isWeather: ringTime["isWeather"] as! Bool,
+                weatherTypes: ringTime["weatherTypes"] as! String
+              )
+              self.scheduleNotification(
+                uuid: ringTime["alarmID"] as! String,
+                title: "Test Alarm",
+                body: "Don't wake up, it's a test alarm",
+                interval: ringTime["interval"] as! Double
+              )
+              
+              result(nil)
+          case "cancelAllScheduledAlarms":
+              NSLog("Log:FLUTTER CALLED CANCEL ALARMS")
+              self.cancelAllScheduledAlarms()
+              result(nil)
+          case "bringAppToForeground":
+              NSLog("Log:BRING APP FOREGROUND")
+              //self.bringAppToForeground()
+              result(nil)
+          case "minimizeApp":
+              NSLog("Log:APP MINIMIZED")
+              //self.minimizeApp()
+              result(nil)
+          case "playDefaultAlarm":
+              self.playDefaultAlarm()
+              result(nil)
+          case "stopDefaultAlarm":
+              self.stopDefaultAlarm()
+              result(nil)
+          default:
+              result(FlutterMethodNotImplemented)
           }
       }
       
@@ -71,11 +108,11 @@ import UserNotifications
               result(nil)
           case "runtimerNotif":
               // TODO
-              print("RUNTIMER NOTIF CALLED")
+              NSLog("Log:RUNTIMER NOTIF CALLED")
               result(nil)
           case "clearTimerNotif":
               // TODO
-              print("CLEAR TIMER NOTIF")
+              NSLog("Log:CLEAR TIMER NOTIF")
               result(nil)
           default:
               result(FlutterMethodNotImplemented)
@@ -86,39 +123,88 @@ import UserNotifications
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
     
+//    override func userNotificationCenter(_ center: UNUserNotificationCenter,
+//                                willPresent notification: UNNotification,
+//                                withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+//        self.playDefaultAlarm()
+//        completionHandler([.banner, .sound])
+//    }
+    
     private func playDefaultAlarm() {
-            guard let soundURL = Bundle.main.url(forResource: "digialarm", withExtension: "mp3") else {
-                print("Alarm sound file not found")
-                return
-            }
-
-            do {
-                AppDelegate.audioPlayer = try AVAudioPlayer(contentsOf: soundURL)
-                AppDelegate.audioPlayer?.numberOfLoops = -1 // Loop indefinitely
-                AppDelegate.audioPlayer?.prepareToPlay()
-                AppDelegate.audioPlayer?.play()
-            } catch {
-                print("Error playing alarm sound: \(error.localizedDescription)")
-            }
-            print("TIMER ALERT PLAYING")
+        guard let soundURL = Bundle.main.url(forResource: "digialarm", withExtension: "wav") else {
+            NSLog("Log:Alarm sound file not found")
+            return
         }
 
-        private func stopDefaultAlarm() {
-            AppDelegate.audioPlayer?.stop()
-            print("TIMER ALERT STOPPED")
+        do {
+            AppDelegate.audioPlayer = try AVAudioPlayer(contentsOf: soundURL)
+            AppDelegate.audioPlayer?.numberOfLoops = -1 // Loop indefinitely
+            AppDelegate.audioPlayer?.prepareToPlay()
+            AppDelegate.audioPlayer?.play()
+        } catch {
+            NSLog("Log:Error playing alarm sound: \(error.localizedDescription)")
         }
+        NSLog("Log:TIMER ALERT PLAYING")
+    }
 
-        private func requestNotificationAuth() {
-            AppDelegate.userNotification.requestAuthorization(options: [.alert, .sound, .badge]) {(authorized, error) in
-                if authorized {
-                    print("NOTIFICATION AUTHORIZED")
-                }
-                else if !authorized {
-                    print("NOT AUTHORIZED")
-                }
-                else {
-                    print(error?.localizedDescription as Any)
-                }
+    private func stopDefaultAlarm() {
+        AppDelegate.audioPlayer?.stop()
+        NSLog("Log:TIMER ALERT STOPPED")
+    }
+
+    private func requestNotificationAuth() {
+        AppDelegate.userNotification.requestAuthorization(options: [.alert, .sound, .badge]) {(authorized, error) in
+            if authorized {
+                NSLog("Log:NOTIFICATION AUTHORIZED")
+            }
+            else if !authorized {
+                NSLog("Log:NOT AUTHORIZED")
+            }
+            else {
+                NSLog("Log:ERROR NOTIFICATION ACCESS")
+                print(error?.localizedDescription as Any)
             }
         }
+    }
+    
+    private func scheduleAlarm(
+        alarmID: String,
+        interval: Double,
+        isActivity: Bool,
+        isLocation: Bool,
+        location: String,
+        isWeather: Bool,
+        weatherTypes: String
+    ) {}
+    
+    private func cancelAllScheduledAlarms() {}
+    
+    private func scheduleNotification(
+        uuid: String,
+        title: String,
+        body: String,
+        interval: Double
+    ) {
+        let content = UNMutableNotificationContent()
+        content.title = title
+        content.body = body
+        //content.sound = UNNotificationSound.default
+        content.sound = UNNotificationSound(named: UNNotificationSoundName("newday.wav"))
+        // intereption level only ios >= 15.0
+        content.interruptionLevel = UNNotificationInterruptionLevel.timeSensitive
+        
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: interval, repeats: false)
+        
+        let request = UNNotificationRequest(identifier: uuid, content: content, trigger: trigger)
+        
+        do {
+            try AppDelegate.userNotification.add(request)
+        } catch {
+            NSLog("Log:ERROR SCHEDULING NOTIFICATION AT INTERVAL: \(interval)")
+            return
+        }
+        
+        NSLog("Log:ALARM SCHEDULED AFTER: \(interval) seconds")
+    }
 }
