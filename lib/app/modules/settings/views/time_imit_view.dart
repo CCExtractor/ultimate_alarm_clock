1fff9bb3df159fe01341be8f2e22345a55ed2fb4
mahendra kasula <kasulamahi624@gmail.com>
2025-03-10 19:25:28 +0530

    Create time_imit_view.dart

---
+++ lib/app/modules/settings/views/time_imit_view.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:ultimate_alarm_clock/app/utils/constants.dart';
+import 'package:ultimate_alarm_clock/app/utils/utils.dart';
+import '../controllers/settings_controller.dart';
+import '../controllers/theme_controller.dart';
+
+class TimeLimitView extends StatelessWidget {
+  TimeLimitView({
+    Key? key,
+    required this.themeController,
+    required this.height,
+    required this.width,
+  }) : super(key: key);
+
+  final ThemeController themeController;
+  final SettingsController controller = Get.find<SettingsController>();
+  final double width;
+  final double height;
+
+  @override
+  Widget build(BuildContext context) {
+    int timeLimit;
+    return Obx(
+      () => Material(
+        color: Colors.transparent,
+        child: InkWell(
+          onTap: () {
+            Utils.hapticFeedback();
+            timeLimit = controller.challengeTimeLimit.value;
+            Get.defaultDialog(
+              onWillPop: () async {
+                Get.back();
+                controller.challengeTimeLimit.value = timeLimit;
+                return true;
+              },
+              titlePadding: const EdgeInsets.symmetric(vertical: 20, horizontal: 5),
+              backgroundColor: themeController.secondaryBackgroundColor.value,
+              title: 'Customize Challenge Timer'.tr,
+              titleStyle: Theme.of(context).textTheme.displaySmall,
+              content: Obx(
+                () => Column(
+                  children: [
+                    Text(
+                      '${controller.challengeTimeLimit.value} seconds'.tr,
+                      style: Theme.of(context).textTheme.displaySmall,
+                    ),
+                    Slider(
+                      value: controller.challengeTimeLimit.value.toDouble(),
+                      onChanged: (double value) {
+                        controller.challengeTimeLimit.value = value.toInt();
+                      },
+                      min: 5.0,
+                      max: 60.0,
+                      divisions: 55,
+                      label: controller.challengeTimeLimit.value.toString(),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        controller.saveSettings();
+                        Get.back();
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: kprimaryColor,
+                      ),
+                      child: Text(
+                        'Apply Time Limit'.tr,
+                        style: TextStyle(
+                          color: themeController.secondaryTextColor.value,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+          child: Container(
+            width: width * 0.91,
+            height: height * 0.09,
+            decoration: Utils.getCustomTileBoxDecoration(
+              isLightMode: themeController.currentTheme.value == ThemeMode.light,
+            ),
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 10, right: 10),
+                child: ListTile(
+                  tileColor: themeController.secondaryBackgroundColor.value,
+                  title: Text(
+                    'Challenge Timer Limit'.tr,
+                    style: TextStyle(
+                      color: themeController.primaryTextColor.value,
+                      fontSize: 15,
+                    ),
+                  ),
+                  trailing: Wrap(
+                    crossAxisAlignment: WrapCrossAlignment.center,
+                    children: [
+                      Obx(
+                        () => Text(
+                          '${controller.challengeTimeLimit.value.round().toInt()} seconds',
+                          style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                            color: themeController.primaryTextColor.value,
+                            fontSize: 13,
+                          ),
+                        ),
+                      ),
+                      Icon(
+                        Icons.chevron_right,
+                        color: themeController.primaryDisabledTextColor.value,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
