45367044e30e58ccded64c254479343e7a183ccc
Shubhang Dixit <143957984+shubhang-d@users.noreply.github.com>
2025-03-12 22:15:42 +0530

    Updated timer_view.dart by removing redundant comments

--- lib/app/modules/timer/views/timer_view.dart
+++ lib/app/modules/timer/views/timer_view.dart
@@ -785,8 +785,7 @@ class TimerView extends GetView<TimerController> {
                                 controller.hours.value = 0;
                                 controller.minutes.value = 1;
                                 controller.seconds.value = 0;
-                                inputTimeController.setTextFieldTimerTime(); //Sets the input field to initial 1 minute, 
-                                                                             //after setting the timer
+                                inputTimeController.setTextFieldTimerTime(); 
 
                                 Get.back();
                               },
