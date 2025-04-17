1661bf7bb3ab7eb40ae034a0c4adb28644cd63c7
mahendra-918 <kasulamahi624@gmail.com>
2025-04-17 22:02:59 +0530

    Fix: Blank space appearing above timer after adding multiple laps in stopwatch

--- lib/app/modules/stopwatch/controllers/stopwatch_controller.dart
+++ lib/app/modules/stopwatch/controllers/stopwatch_controller.dart
@@ -19,11 +19,18 @@ class StopwatchController extends GetxController {
     if (_stopwatch.isRunning) {
       final currentTime = _stopwatch.elapsed;
       final lapTime = currentTime - _lastFlagTime;
-      hasFlags.value = true;
+      
+      // First add the flag
       flags.add(Flag(
           number: flags.length + 1, lapTime: lapTime, totalTime: currentTime));
-      listKey.currentState
-          ?.insertItem(0, duration: const Duration(milliseconds: 300));
+      
+      // Then set flags value to true and animate
+      hasFlags.value = true;
+      listKey.currentState?.insertItem(
+        0, 
+        duration: const Duration(milliseconds: 150),
+      );
+      
       _lastFlagTime = currentTime;
     }
   }
